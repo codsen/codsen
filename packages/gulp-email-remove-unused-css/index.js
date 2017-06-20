50e3c18a1701927d6d204f66134b8e6689fd47e2
Roy Reveltas <roy@codeandsend.com>
2017-06-20 11:48:18 +0100

    Chore: deps update

--- packages/gulp-email-remove-unused-css/index.js
+++ packages/gulp-email-remove-unused-css/index.js
@@ -8,7 +8,7 @@ var PLUGIN_NAME = 'gulp-email-remove-unused-css'
 
 module.exports = function (options) {
   var stream = new Transform({objectMode: true})
-  stream._transform = function (file, encoding, cb) {
+  stream._transform = (file, encoding, cb) => {
     if (file.isStream()) {
       var error = 'Streaming not supported'
       return cb(new gutil.PluginError(PLUGIN_NAME, error))
@@ -18,7 +18,7 @@ module.exports = function (options) {
         // Don't crash on empty files
         return cb(null, file)
       }
-      file.contents = new Buffer(remove(contents, options)[0])
+      file.contents = Buffer.from(remove(contents, options)[0])
       cb(null, file)
     } else {
       // Pass through when null
