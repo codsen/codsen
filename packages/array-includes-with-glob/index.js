943ab11fb7a457256c6b4a4eb7335cf445be2ed2
Roy Revelt <roy@codsen.com>
2017-08-24 23:07:58 +0100

    small rebase to follow ES6 better

--- packages/array-includes-with-glob/index.js
+++ packages/array-includes-with-glob/index.js
@@ -1,12 +1,12 @@
 'use strict'
-var clone = require('lodash.clonedeep')
-var matcher = require('matcher')
+const clone = require('lodash.clonedeep')
+const matcher = require('matcher')
+const isArr = Array.isArray
 
 function includesWithGlob (originalInput, stringToFind, opts) {
   //
   // internal f()'s
   function existy (x) { return x != null }
-  function isArr (something) { return Array.isArray(something) }
   function isStr (something) { return typeof something === 'string' }
 
   var defaults = {
@@ -32,7 +32,7 @@ function includesWithGlob (originalInput, stringToFind, opts) {
   if (!isStr(stringToFind) && !isArr(stringToFind)) {
     throw new Error('array-includes-with-glob/includesWithGlob(): [THROW_ID_04] second argument must be a string or array of strings! It was given as ' + typeof stringToFind)
   }
-  if (opts.arrayVsArrayAllMustBeFound !== 'any' && opts.arrayVsArrayAllMustBeFound !== 'all') {
+  if ((opts.arrayVsArrayAllMustBeFound !== 'any') && (opts.arrayVsArrayAllMustBeFound !== 'all')) {
     throw new Error('array-includes-with-glob/includesWithGlob(): [THROW_ID_05] opts.arrayVsArrayAllMustBeFound was customised to an unrecognised value, ' + opts.arrayVsArrayAllMustBeFound + '. It must be equal to either "any" or "all".')
   }
 
@@ -42,9 +42,7 @@ function includesWithGlob (originalInput, stringToFind, opts) {
   }
 
   // prevent any mutation + filter out undefined and null elements:
-  var input = clone(originalInput).filter(function (elem) {
-    return existy(elem)
-  })
+  var input = clone(originalInput).filter(elem => existy(elem))
 
   // if array contained only null/undefined values, do a Dutch leave:
   if (input.length === 0) {
@@ -52,23 +50,13 @@ function includesWithGlob (originalInput, stringToFind, opts) {
   }
 
   if (isStr(stringToFind)) {
-    return input.some(function (val) {
-      return matcher.isMatch(val, stringToFind)
-    })
+    return input.some(val => matcher.isMatch(val, stringToFind))
   } else {
     // array then.
     if (opts.arrayVsArrayAllMustBeFound === 'any') {
-      return stringToFind.some(function (stringToFindVal) {
-        return input.some(function (val) {
-          return matcher.isMatch(val, stringToFindVal)
-        })
-      })
+      return stringToFind.some(stringToFindVal => input.some(val => matcher.isMatch(val, stringToFindVal)))
     } else {
-      return stringToFind.every(function (stringToFindVal) {
-        return input.some(function (val) {
-          return matcher.isMatch(val, stringToFindVal)
-        })
-      })
+      return stringToFind.every(stringToFindVal => input.some(val => matcher.isMatch(val, stringToFindVal)))
     }
   }
 }
