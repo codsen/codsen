088e537ed771c8c487bc6253aab9b831653e0d1a
Roy Revelt <roy@codsen.com>
2017-08-26 15:06:44 +0100

    init

---
+++ packages/csv-sort-cli/cli.js
@@ -0,0 +1,220 @@
+#!/usr/bin/env node
+'use strict'
+
+// VARS
+// -----------------------------------------------------------------------------
+
+const chalk = require('chalk')
+const csvSort = require('csv-sort')
+const fs = require('fs')
+const globby = require('globby')
+const inquirer = require('inquirer')
+const log = console.log
+const meow = require('meow')
+const path = require('path')
+const updateNotifier = require('update-notifier')
+const pullAll = require('lodash.pullall')
+const uniq = require('lodash.uniq')
+const isArr = Array.isArray
+
+var state = {}
+state.toDoList = [] // default
+state.overwrite = false // default
+var ui = new inquirer.ui.BottomBar()
+var cli = meow(`
+  Usage
+    $ csvsort YOURFILE.csv
+  or, just type "csvsort" and it will let you pick a file.
+
+  Options
+    -o, --overwrite   Will overwrite the target file instead
+    -h, --help        Shows this help
+    -v, --version     Shows the version of your csv-sort-cli
+
+  Example
+    Just call it in the root, where your csv file is located
+`, {
+  alias: {
+    o: 'overwrite'
+  }
+})
+updateNotifier({pkg: cli.pkg}).notify()
+
+// FUNCTIONS
+// -----------------------------------------------------------------------------
+
+function isStr (something) { return typeof something === 'string' }
+
+// consumes a plain object: {
+//   toDoList - array,
+//   overwrite - boolean
+// }
+function offerAListOfCSVsToPickFrom (stateObj) {
+  // this means, it was called without any arguments.
+  // that's fine
+  let allCSVsHere = globby.sync('./*.csv')
+  if (allCSVsHere.length === 0) {
+    // log(chalk.red(`\ncsv-sort-cli: Alas, computer couldn't find any CSV files in this folder and bailed on us!`))
+    return Promise.reject(new Error(`\ncsv-sort-cli: Alas, computer couldn't find any CSV files in this folder and bailed on us!`))
+  }
+  ui.log.write(chalk.grey('To quit, press CTRL+C'))
+  let questions = [
+    {
+      type: 'list',
+      name: 'file',
+      message: 'Which CSV would you like to check?',
+      choices: allCSVsHere
+    }
+  ]
+  if (
+    (stateObj === undefined) ||
+    !stateObj.hasOwnProperty('overwrite') ||
+    (stateObj.hasOwnProperty('overwrite') && (stateObj.overwrite === false)) ||
+    (typeof stateObj.overwrite !== 'boolean')
+  ) {
+    questions.push({
+      type: 'list',
+      name: 'overwrite',
+      message: `Do you want to overwrite this file with a sorted result?`,
+      choices: [
+        { name: 'yes', value: true },
+        { name: 'no', value: false }
+      ]
+    })
+  }
+  ui.log.write(chalk.yellow('Please pick a file:'))
+  return inquirer.prompt(questions).then(answer => {
+    return {
+      toDoList: [path.basename(answer.file)],
+      overwrite: answer.overwrite || false
+    }
+  })
+}
+
+// Step #0. take care of -v and -h flags that are left out in meow.
+// -----------------------------------------------------------------------------
+
+if (cli.flags.v) {
+  log(cli.pkg.version)
+  process.exit(0)
+} else if (cli.flags.h) {
+  log(cli.help)
+  process.exit(0)
+}
+
+// Step #1. gather the to-do list of files.
+// -----------------------------------------------------------------------------
+
+if (cli.input.length > 0) {
+  state.toDoList = cli.input
+}
+
+// if --overwrite/-o flag is used, the following argument will be put as flag's value, not in "cli.input[]":
+// we anticipate the can be multiple, potentially-false flags mixed with valid file names
+if (Object.keys(cli.flags).length !== 0) {
+  // each non-boolean cli.flags value must be added to the `toDoList`
+  Object.keys(cli.flags).forEach(function (key) {
+    if (typeof cli.flags[key] !== 'boolean') {
+      if (!isArr(cli.flags[key])) {
+        state.toDoList.push(cli.flags[key])
+      } else {
+        state.toDoList = state.toDoList.concat(cli.flags[key].filter(val => isStr(val)))
+      }
+    }
+  })
+  state.toDoList = uniq(state.toDoList)
+}
+
+if ((Object.keys(cli.flags) !== 0) && cli.flags.hasOwnProperty('overwrite')) {
+  // variables that can be misinterpreted as falsey, yet the flag still be in
+  // for example, in "csvsort -o false simples.csv simples2.csv", the cli.flags.overwrite === false (WTF?)
+  state.overwrite = true // we normalise the flag since its value in CLI can precede
+}
+
+// Step #2. create a promise variable and assign it to one of the promises,
+// depending on was the acceptable file passed via args or queries afterwards.
+// -----------------------------------------------------------------------------
+var thePromise
+if ((state.toDoList.length === 0) && (Object.keys(cli.flags).length === 0)) {
+  // ---------------------------------  1  -------------------------------------
+  // if no arguments were given, offer a list:
+  thePromise = offerAListOfCSVsToPickFrom(state)
+} else if (state.toDoList.map(onePath => path.resolve(onePath)).filter(fs.existsSync).length > 0) {
+  // ---------------------------------  2  -------------------------------------
+  // basically achieving: (!fs.existsSync)
+  let erroneous = pullAll(
+    state.toDoList.map(onePath => path.resolve(onePath)),
+    state.toDoList.map(onePath => path.resolve(onePath)).filter(fs.existsSync)
+  ).map(singlePath => path.basename(singlePath)) // then filtering file names-only
+
+  // write the list of unrecognised file names into the console:
+  if (erroneous.length > 0) {
+    log(chalk.red(`\ncsv-sort-cli: Alas, the following file${(erroneous.length > 1) ? `s don't` : ` doesn't`} exist: "${erroneous.join('", "')}"`))
+  }
+
+  // remove non-existing paths from toDoList:
+  state.toDoList = state.toDoList.map(onePath => path.resolve(onePath)).filter(fs.existsSync)
+
+  // create the final promise variable we're going to use later:
+  thePromise = Promise.resolve(state)
+} else {
+  // ---------------------------------  3  -------------------------------------
+  let butStateWasRecognisedMsg = ''
+  if (state.overwrite) {
+    butStateWasRecognisedMsg = `But it recognised your "-o" flag.`
+  }
+  log(chalk.yellow(`\ncsv-sort-cli: Computer didn't recognise any CSV files in your input!\n${butStateWasRecognisedMsg}`))
+
+  // if there were no valid path in the arguments, query the files from the
+  // existing CSV's in the current folder:
+  thePromise = offerAListOfCSVsToPickFrom(state)
+}
+
+// Step #3.
+// -----------------------------------------------------------------------------
+
+thePromise
+  .then(receivedState => {
+    receivedState.toDoList.map(requestedCSVsPath => {
+      // read the source
+      fs.readFile(requestedCSVsPath, 'utf8', (csvError, csvData) => {
+        if (csvData) {
+          try {
+            let cleaned = csvSort(csvData)
+            if (receivedState.overwrite) {
+              // overwrite
+              fs.writeFile(path.basename(requestedCSVsPath), cleaned.res.join('\n'), 'utf8', (err) => {
+                if (err) throw err
+                log(chalk.green(`csv-sort-cli: Yay! The ${path.basename(requestedCSVsPath)} has been fixed and overwritten! Check it out.`))
+                process.exit(0)
+              })
+            } else {
+              // create a new file with appended hyphen+integer before extension
+              var proposedNewFileName
+              for (let i = 1; i < 1001; i++) {
+                proposedNewFileName = path.basename(requestedCSVsPath, path.extname(requestedCSVsPath)) + '-' + i + path.extname(requestedCSVsPath)
+                if (!fs.existsSync(path.resolve(proposedNewFileName))) {
+                  fs.writeFile(proposedNewFileName, cleaned.res.join('\n'), 'utf8', (err) => {
+                    if (err) throw err
+                    log(chalk.green(`csv-sort-cli: Yay! A new file, ${proposedNewFileName} has been created! Check it out.`))
+                    process.exit(0)
+                  })
+                  break
+                }
+              }
+              path.basename(requestedCSVsPath, path.extname(requestedCSVsPath))
+            }
+          } catch (e) {
+            return Promise.reject(new Error(`\ncsv-sort-cli: Alas, computer encountered an error:\n${e}`))
+          }
+        }
+        if (csvError) {
+          return Promise.reject(new Error(`\ncsv-sort-cli: Alas, computer couldn't fetch the file "${path.basename(requestedCSVsPath)}" you requested and bailed on us!`))
+        }
+      })
+    })
+  })
+  .catch(err => {
+    log(chalk.red(err))
+    process.exit(1)
+  })
---
+++ packages/csv-sort-cli/readme.md
@@ -0,0 +1,141 @@
+# csv-sort-cli
+
+<a href="https://standardjs.com" style="float: right; padding: 0 0 20px 20px;"><img src="https://cdn.rawgit.com/feross/standard/master/sticker.svg" alt="Standard JavaScript" width="100" align="right"></a>
+
+> Sorts double-entry CSVs coming from internet banking statements (and accounting, in general)
+
+[![Link to npm page][npm-img]][npm-url]
+[![Build Status][travis-img]][travis-url]
+[![bitHound Score][bithound-img]][bithound-url]
+[![bitHound Dependencies][deps-img]][deps-url]
+[![bitHound Dev Dependencies][dev-img]][dev-url]
+[![Known Vulnerabilities][vulnerabilities-img]][vulnerabilities-url]
+[![Downloads/Month][downloads-img]][downloads-url]
+[![View dependencies as 2D chart][deps2d-img]][deps2d-url]
+
+## Table of Contents
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+
+
+- [Install globally](#install-globally)
+- [What it does exactly](#what-it-does-exactly)
+- [Updating it](#updating-it)
+- [Contributing](#contributing)
+- [Licence](#licence)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
+**[⬆ &nbsp;back to top](#)**
+
+## Install globally
+
+```bash
+$ npm i -g csv-sort-cli
+```
+
+- then, either call `csvsort` appending your file name (with or without `-o`/`--overwrite` flag):
+
+```bash
+$ csvsort YOURFILE.csv
+
+$ csvsort -o YOURFILE.csv
+$ csvsort --overwrite YOURFILE.csv
+$ csvsort -v
+$ csvsort --version
+$ csvsort -h
+$ csvsort --help
+```
+
+- or omit the file name and `csv-sort-cli` will offer a list of CSV files in the current folder to choose from:
+
+```bash
+$ csvsort
+```
+
+You can even try it without installing — use `npx`:
+
+```bash
+$ npx csv-sort-cli YOURFILE.csv
+```
+
+**[⬆ &nbsp;back to top](#)**
+
+## What it does exactly
+
+1. It **sorts CSV file rows** to correspond to the double-entry bookkeeping format:
+
+![double bookkeeping example](https://cdn.rawgit.com/codsen/csv-sort/e273cf48/media/img1.png)
+
+Sometimes Internet Banking CSV's have rows in a wrong order.
+
+2. As a bonus, it will **trim** the empty columns/rows:
+
+![2D trim of a CSV contents](https://cdn.rawgit.com/codsen/csv-sort/2bdf5256/media/img2.png)
+
+3. Not to mention, the [our custom CSV parse](https://github.com/codsen/csv-split-easy) used here will ensure that all CSV cell _contents_ are trimmed and there are no empty rows between the content rows. It also accepts any commas as content if the cell is wrapped with double quotes. Read more in [its repo's readme](https://github.com/codsen/csv-split-easy).
+
+**[⬆ &nbsp;back to top](#)**
+
+## Updating it
+
+When you install it globally, it will check occasionally, are there newer versions available, and if so, will show a message nagging you to update. It's the [same tool](https://www.npmjs.com/package/update-notifier) that AVA and [npm](https://www.npmjs.com/package/npm) themselves use!
+
+**[⬆ &nbsp;back to top](#)**
+
+## Contributing
+
+All contributions are welcome. Please stick to [Standard JavaScript](https://standardjs.com) notation.
+
+If you see anything incorrect whatsoever, do [raise an issue](https://github.com/codsen/csv-sort-cli/issues). If you have any comments on the code, including ideas how to improve something, don't hesitate to contact me by email, Twitter or raise an issue on a mentioned issue link.
+
+**[⬆ &nbsp;back to top](#)**
+
+## Licence
+
+> MIT License (MIT)
+
+> Copyright (c) 2017 Codsen Ltd, Roy Revelt
+
+> Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+> The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+[npm-img]: https://img.shields.io/npm/v/csv-sort-cli.svg
+[npm-url]: https://www.npmjs.com/package/csv-sort-cli
+
+[travis-img]: https://travis-ci.org/codsen/csv-sort-cli.svg?branch=master
+[travis-url]: https://travis-ci.org/codsen/csv-sort-cli
+
+[bithound-img]: https://www.bithound.io/github/codsen/csv-sort-cli/badges/score.svg
+[bithound-url]: https://www.bithound.io/github/codsen/csv-sort-cli
+
+[deps-img]: https://www.bithound.io/github/codsen/csv-sort-cli/badges/dependencies.svg
+[deps-url]: https://www.bithound.io/github/codsen/csv-sort-cli/master/dependencies/npm
+
+[dev-img]: https://www.bithound.io/github/codsen/csv-sort-cli/badges/devDependencies.svg
+[dev-url]: https://www.bithound.io/github/codsen/csv-sort-cli/master/dependencies/npm
+
+[downloads-img]: https://img.shields.io/npm/dm/csv-sort-cli.svg
+[downloads-url]: https://www.npmjs.com/package/csv-sort-cli
+
+[vulnerabilities-img]: https://snyk.io/test/github/codsen/csv-sort-cli/badge.svg
+[vulnerabilities-url]: https://snyk.io/test/github/codsen/csv-sort-cli
+
+[deps2d-img]: https://img.shields.io/badge/deps%20in%202D-see_here-08f0fd.svg
+[deps2d-url]: http://npm.anvaka.com/#/view/2d/csv-sort-cli
