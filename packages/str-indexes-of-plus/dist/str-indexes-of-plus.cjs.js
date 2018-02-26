2f9d28f5e3cbd78c4ddd3389958a82c419a4e93d
Roy Revelt <roy@codsen.com>
2018-02-26 01:32:12 +0000

    update dependencies

--- packages/str-indexes-of-plus/dist/str-indexes-of-plus.cjs.js
+++ packages/str-indexes-of-plus/dist/str-indexes-of-plus.cjs.js
@@ -8,8 +8,6 @@ var isNumStr = _interopDefault(require('is-natural-number-string'));
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-/* eslint no-param-reassign:0 */
-
 function existy(x) {
   return x != null;
 }
--- packages/str-indexes-of-plus/dist/str-indexes-of-plus.esm.js
+++ packages/str-indexes-of-plus/dist/str-indexes-of-plus.esm.js
@@ -4,8 +4,6 @@ import isNumStr from 'is-natural-number-string';
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-/* eslint no-param-reassign:0 */
-
 function existy(x) {
   return x != null;
 }
