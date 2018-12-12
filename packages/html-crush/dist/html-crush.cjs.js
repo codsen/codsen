6ebb6cbf531602b0534772d3ad87b64375e10005
Roy Revelt <roy@codsen.com>
2018-12-12 07:10:05 +0000

    0.2.0

--- packages/html-crush/dist/html-crush.cjs.js
+++ packages/html-crush/dist/html-crush.cjs.js
@@ -84,7 +84,7 @@ function _nonIterableRest() {
 }
 
 var name = "html-crush";
-var version = "0.1.3";
+var version = "0.2.0";
 var description = "Minifies HTML, accepts HTML, mixed with other sources";
 var license = "MIT";
 var engines = {
