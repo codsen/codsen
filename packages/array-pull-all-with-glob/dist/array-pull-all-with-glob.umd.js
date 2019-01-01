58f16d8cb8941be1e7c0ab3fed51b2ac3ac8b38f
Roy Revelt <roy@codsen.com>
2019-01-01 16:19:29 +0000

    build: Add built files

--- packages/array-pull-all-with-glob/dist/array-pull-all-with-glob.umd.js
+++ packages/array-pull-all-with-glob/dist/array-pull-all-with-glob.umd.js
@@ -1,7 +1,7 @@
 /**
  * array-pull-all-with-glob
  * pullAllWithGlob - like _.pullAll but pulling stronger, with globs
- * Version: 4.8.6
+ * Version: 4.8.7
  * Author: Roy Revelt, Codsen Ltd
  * License: MIT
  * Homepage: https://bitbucket.org/codsen/codsen/src/master/packages/array-pull-all-with-glob
