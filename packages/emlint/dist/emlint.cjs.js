2a910df94d717bfeb0c8d95264c9ade08a4b7850
Roy Revelt <roy@codsen.com>
2019-03-23 14:00:54 +0000

    [skip ci] chore: Release
     - emlint@1.2.7

--- packages/emlint/dist/emlint.cjs.js
+++ packages/emlint/dist/emlint.cjs.js
@@ -1,7 +1,7 @@
 /**
  * emlint
  * Non-parsing, email template-oriented linter
- * Version: 1.2.6
+ * Version: 1.2.7
  * Author: Roy Revelt, Codsen Ltd
  * License: MIT
  * Homepage: https://gitlab.com/codsen/codsen/tree/master/packages/emlint
@@ -187,7 +187,7 @@ var knownHTMLTags = [
 	"xml"
 ];
 
-var version = "1.2.6";
+var version = "1.2.7";
 
 var errorsRules = {
 	"bad-named-html-entity-multiple-encoding": {
