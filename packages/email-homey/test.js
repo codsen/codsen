396313e7fa1e30ae57bfacd3732de943c5d2bed3
Roy Revelt <roy@codsen.com>
2019-06-28 21:08:32 +0100

    chore: Update tests to match the new execa api

--- packages/email-homey/test.js
+++ packages/email-homey/test.js
@@ -6,7 +6,7 @@ import path from "path";
 
 test.serial("generates the homepage with correct folders", async t => {
   await fs.unlink("./fixtures/index.html", () => {});
-  await execa.stdout("./cli.js", ["fixtures"]);
+  await execa("./cli.js", ["fixtures"]);
   t.deepEqual(
     fs.readFileSync(path.join(__dirname, "fixtures/index.html"), "utf8"),
     fs.readFileSync(path.join(__dirname, "fixtures/reference.html"), "utf8")
@@ -16,7 +16,7 @@ test.serial("generates the homepage with correct folders", async t => {
 
 test.serial("unused flags are OK", async t => {
   await fs.unlink("./fixtures/index.html", () => {});
-  await execa.stdout("./cli.js", ["-x", "-y", "-z", "fixtures"]);
+  await execa("./cli.js", ["-x", "-y", "-z", "fixtures"]);
   t.deepEqual(
     fs.readFileSync(path.join(__dirname, "fixtures/index.html"), "utf8"),
     fs.readFileSync(path.join(__dirname, "fixtures/reference.html"), "utf8")
@@ -26,36 +26,44 @@ test.serial("unused flags are OK", async t => {
 
 test.serial("empty input", async t => {
   // default mode - says nothing
-  t.is(await execa.stdout("./cli.js"), "");
+  t.is(await execa("./cli.js").then(obj => obj.stdout), "");
 
   // loud mode - complains:
-  t.regex(await execa.stdout("./cli.js", ["-l"]), /nothing to work with/i);
+  t.regex(
+    await execa("./cli.js", ["-l"]).then(obj => obj.stdout),
+    /nothing to work with/i
+  );
 });
 
 test.serial("too many directories given", async t => {
   // default mode - says nothing
   t.regex(
-    await execa.stdout("./cli.js", ["fixtures", "fixtures2"]),
+    await execa("./cli.js", ["fixtures", "fixtures2"]).then(obj => obj.stdout),
     /too many/i
   );
   t.regex(
-    await execa.stdout("./cli.js", ["fixtures", "-l", "fixtures2"]),
+    await execa("./cli.js", ["fixtures", "-l", "fixtures2"]).then(
+      obj => obj.stdout
+    ),
     /too many/i
   );
   t.regex(
-    await execa.stdout("./cli.js", [
+    await execa("./cli.js", [
       "fixtures",
       "-l",
       "-x",
       "-y",
       "-z",
       "fixtures2"
-    ]),
+    ]).then(obj => obj.stdout),
     /too many/i
   );
 });
 
 test.serial("help and version flags work", async t => {
-  t.regex(await execa.stdout("./cli.js", ["-v"]), /\d\.\d\.\d/i);
-  t.regex(await execa.stdout("./cli.js", ["-h"]), /usage/i);
+  t.regex(
+    await execa("./cli.js", ["-v"]).then(obj => obj.stdout),
+    /\d\.\d\.\d/i
+  );
+  t.regex(await execa("./cli.js", ["-h"]).then(obj => obj.stdout), /usage/i);
 });
