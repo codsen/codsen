667738302cf6fc6feddaf43474363aa8cddf9b70
Roy Revelt <roy@codsen.com>
2019-09-27 06:24:24 +0100

    chore: setup refresh

--- packages/ranges-crop/rollup.config.js
+++ packages/ranges-crop/rollup.config.js
@@ -42,7 +42,7 @@ export default commandLineArgs => {
     {
       input: "src/main.js",
       output: [{ file: pkg.main, format: "cjs" }],
-      external: ["is-natural-number", "ordinal-number-suffix", "ranges-merge"],
+      external: ["is-natural-number", "ranges-merge"],
       plugins: [
         strip({
           sourceMap: false
@@ -58,7 +58,7 @@ export default commandLineArgs => {
     {
       input: "src/main.js",
       output: [{ file: pkg.module, format: "es" }],
-      external: ["is-natural-number", "ordinal-number-suffix", "ranges-merge"],
+      external: ["is-natural-number", "ranges-merge"],
       plugins: [
         strip({
           sourceMap: false
