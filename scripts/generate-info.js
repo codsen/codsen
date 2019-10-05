79887a9813f254b959b169927d81ff3b9bd12b4d
Roy Revelt <roy@codsen.com>
2019-10-05 20:15:12 +0100

    build: add dependency usage reporting

--- scripts/generate-info.js
+++ scripts/generate-info.js
@@ -2,6 +2,9 @@
 
 const fs = require("fs");
 const path = require("path");
+const {
+  sortAllObjectsSync
+} = require("../packages/json-comb-core/dist/json-comb-core.cjs");
 
 const dateObj = new Date();
 const month = dateObj.getUTCMonth() + 1; //months from 1-12
@@ -54,7 +57,7 @@ const allPackages = fs
 
 const interdep = [];
 
-// 1. Assemble a JSON of all package and their deps
+// 1. Assemble a JSON of all packages and their deps
 // -----------------------------------------------------------------------------
 
 // {
@@ -66,6 +69,7 @@ const interdep = [];
 // },
 
 const allStats = [];
+const dependencyStats = { dependencies: {}, devDependencies: {} };
 
 allPackages.map(name => {
   const pack = JSON.parse(
@@ -107,6 +111,39 @@ allPackages.map(name => {
     console.log(
       `! couldn't read/parse ${path.join("packages", name, "testStats.json")}`
     );
+  }
+
+  // compile dependency stats
+  if (Object.prototype.hasOwnProperty.call(pack, "dependencies")) {
+    // has deps
+    Object.keys(pack.dependencies).forEach(dep => {
+      // if dependency's name doesn't exist in compiled obj., create key
+      if (
+        !Object.prototype.hasOwnProperty.call(dependencyStats.dependencies, dep)
+      ) {
+        dependencyStats.dependencies[dep] = 1;
+      } else {
+        dependencyStats.dependencies[dep] =
+          dependencyStats.dependencies[dep] + 1;
+      }
+    });
+  }
+  if (Object.prototype.hasOwnProperty.call(pack, "devDependencies")) {
+    // has deps
+    Object.keys(pack.devDependencies).forEach(dep => {
+      // if devdependency's name doesn't exist in compiled obj., create key
+      if (
+        !Object.prototype.hasOwnProperty.call(
+          dependencyStats.devDependencies,
+          dep
+        )
+      ) {
+        dependencyStats.devDependencies[dep] = 1;
+      } else {
+        dependencyStats.devDependencies[dep] =
+          dependencyStats.devDependencies[dep] + 1;
+      }
+    });
   }
 });
 
@@ -162,12 +199,70 @@ compiledStats.testsCountMedian = median(
 );
 
 // console.log(
-//   `109 generate-info.js: ${`\u001b[${33}m${`compiledStats`}\u001b[${39}m`} = ${JSON.stringify(
+//   `202 generate-info.js: ${`\u001b[${33}m${`compiledStats`}\u001b[${39}m`} = ${JSON.stringify(
 //     compiledStats,
 //     null,
 //     4
 //   )}`
 // );
+
+// 3. compile top 10 of own and external deps and devdeps
+// -----------------------------------------------------------------------------
+
+const top10OwnDeps = [];
+const top10ExternalDeps = [];
+// there are no devdeps statistics because all devdeps are the same
+
+let foundOwnMax;
+let foundExternalMax;
+for (let i = 0; i < 10; i++) {
+  // reset
+  foundOwnMax = null;
+  foundExternalMax = null;
+
+  // iterate
+  Object.keys(dependencyStats.dependencies).forEach(depName => {
+    if (
+      (!foundOwnMax ||
+        dependencyStats.dependencies[depName] >
+          dependencyStats.dependencies[foundOwnMax]) &&
+      allPackages.includes(depName) &&
+      !top10OwnDeps.some(obj =>
+        Object.prototype.hasOwnProperty.call(obj, depName)
+      )
+    ) {
+      foundOwnMax = depName;
+    }
+
+    if (
+      (!foundExternalMax ||
+        dependencyStats.dependencies[depName] >
+          dependencyStats.dependencies[foundExternalMax]) &&
+      !allPackages.includes(depName) &&
+      !top10ExternalDeps.some(obj =>
+        Object.prototype.hasOwnProperty.call(obj, depName)
+      )
+    ) {
+      foundExternalMax = depName;
+    }
+  });
+  if (foundOwnMax) {
+    top10OwnDeps.push({
+      [foundOwnMax]: dependencyStats.dependencies[foundOwnMax]
+    });
+  }
+  if (foundExternalMax) {
+    top10ExternalDeps.push({
+      [foundExternalMax]: dependencyStats.dependencies[foundExternalMax]
+    });
+  }
+}
+
+dependencyStats.top10OwnDeps = top10OwnDeps;
+dependencyStats.top10ExternalDeps = top10ExternalDeps;
+
+// 4. write files
+// -----------------------------------------------------------------------------
 
 fs.writeFile(
   path.resolve("stats/interdeps.json"),
@@ -203,7 +298,20 @@ fs.writeFile(
   }
 );
 
-// 3. assemble historical unit test totals
+fs.writeFile(
+  path.resolve("stats/dependencyStats.json"),
+  JSON.stringify(sortAllObjectsSync(dependencyStats), null, 4),
+  err => {
+    if (err) {
+      throw err;
+    }
+    console.log(
+      `\u001b[${32}m${`dependencyStats.json written OK`}\u001b[${39}m`
+    );
+  }
+);
+
+// 5. assemble historical unit test totals
 // -----------------------------------------------------------------------------
 
 let oldHistoricTotals;
