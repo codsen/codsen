db6c55c88f05f6d96140533e24bef6befb0c6b19
Roy Revelt <roy@codsen.com>
2019-11-02 12:49:18 +0000

    [skip ci] chore: Release
     - codsen-tokenizer@1.1.0
     - emlint@1.8.4
     - html-crush@1.9.9
     - html-table-patcher@1.1.29
     - is-html-tag-opening@1.1.0
     - lect@0.10.5

--- packages/emlint/dist/emlint.esm.js
+++ packages/emlint/dist/emlint.esm.js
@@ -1,7 +1,7 @@
 /**
  * emlint
  * Non-parsing static code analysis tool for HTML mixed with anything
- * Version: 1.8.3
+ * Version: 1.8.4
  * Author: Roy Revelt, Codsen Ltd
  * License: MIT
  * Homepage: https://gitlab.com/codsen/codsen/tree/master/packages/emlint
@@ -891,7 +891,7 @@ var errorsRules = {
 }
 };
 
-var version = "1.8.3";
+var version = "1.8.4";
 
 const isArr = Array.isArray;
 const lowAsciiCharacterNames = [
