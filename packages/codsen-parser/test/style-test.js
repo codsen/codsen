c344f55d1143d539bd08f4d3cfa923dba3bbfaf3
Roy Revelt <roy@codsen.com>
2020-02-13 06:51:18 +0000

    feat: update to the tokenizer's latest api

--- packages/codsen-parser/test/style-test.js
+++ packages/codsen-parser/test/style-test.js
@@ -36,11 +36,13 @@ t.test(`01.01 - ${`\u001b[${33}m${`style`}\u001b[${39}m`} - two tags`, t => {
             end: 25,
             openingCurlyAt: 12,
             closingCurlyAt: 24,
+            selectorsStart: 8,
+            selectorsEnd: 12,
             selectors: [
               {
                 value: ".red",
-                selectorStart: 8,
-                selectorEnd: 12
+                selectorStarts: 8,
+                selectorEnds: 12
               }
             ]
           },
@@ -72,3 +74,110 @@ t.test(`01.01 - ${`\u001b[${33}m${`style`}\u001b[${39}m`} - two tags`, t => {
   );
   t.end();
 });
+
+// 02. media
+// -----------------------------------------------------------------------------
+
+t.only(
+  `02.01 - ${`\u001b[${36}m${`media`}\u001b[${39}m`} - two selectors with empty curlies`,
+  t => {
+    t.same(
+      cparser(`<style>
+@media screen and {
+.a, .b {}
+}
+</style>`),
+      [
+        {
+          children: [
+            {
+              type: "text",
+              start: 7,
+              end: 8
+            },
+            {
+              type: "text",
+              start: 27,
+              end: 28
+            },
+            {
+              type: "rule",
+              start: 28,
+              end: 37,
+              openingCurlyAt: 35,
+              closingCurlyAt: 36,
+              selectorsStart: 28,
+              selectorsEnd: 34,
+              selectors: [
+                {
+                  value: ".a",
+                  selectorStarts: 28,
+                  selectorEnds: 30
+                },
+                {
+                  value: ".b",
+                  selectorStarts: 32,
+                  selectorEnds: 34
+                }
+              ]
+            },
+            {
+              type: "text",
+              start: 37,
+              end: 38
+            },
+            {
+              type: "at",
+              start: 8,
+              end: 39,
+              identifier: "media",
+              identifierStartsAt: 9,
+              identifierEndsAt: 14,
+              query: "screen and",
+              queryStartsAt: 15,
+              queryEndsAt: 25,
+              openingCurlyAt: 26,
+              closingCurlyAt: 38
+            },
+            {
+              type: "text",
+              start: 39,
+              end: 40
+            }
+          ],
+          type: "tag",
+          start: 0,
+          end: 7,
+          tagNameStartsAt: 1,
+          tagNameEndsAt: 6,
+          tagName: "style",
+          recognised: true,
+          closing: false,
+          void: false,
+          pureHTML: true,
+          esp: [],
+          kind: null,
+          attribs: []
+        },
+        {
+          children: [],
+          type: "tag",
+          start: 40,
+          end: 48,
+          tagNameStartsAt: 42,
+          tagNameEndsAt: 47,
+          tagName: "style",
+          recognised: true,
+          closing: true,
+          void: false,
+          pureHTML: true,
+          esp: [],
+          kind: null,
+          attribs: []
+        }
+      ],
+      "02.01"
+    );
+    t.end();
+  }
+);
