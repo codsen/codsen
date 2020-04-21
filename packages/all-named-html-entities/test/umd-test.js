23e416fe3eca97cf164bd28996d91dca114bd28e
Roy Revelt <roy@codsen.com>
2020-04-21 07:50:15 +0100

    refactor: harden the eslint rules and make all tests pass

--- packages/all-named-html-entities/test/umd-test.js
+++ packages/all-named-html-entities/test/umd-test.js
@@ -1,4 +1,4 @@
-import t from "tap";
+import tap from "tap";
 import {
   allNamedEntities,
   entStartsWith,
@@ -12,56 +12,56 @@ import {
   uncertain,
 } from "../dist/all-named-html-entities.umd";
 
-t.test(`01 - entStartsWith is set`, (t) => {
+tap.test(`01 - entStartsWith is set`, (t) => {
   t.ok(entStartsWith.A.E[0] === "AElig");
   t.ok(entStartsWith.A.E[1] === undefined);
   t.end();
 });
 
-t.test(`02 - entStartsWithCaseInsensitive is set`, (t) => {
+tap.test(`02 - entStartsWithCaseInsensitive is set`, (t) => {
   // it's not a real entity:
   t.ok(entStartsWithCaseInsensitive.a.e[0] === "aelig");
   t.ok(entStartsWithCaseInsensitive.a.e[1] === undefined);
   t.end();
 });
 
-t.test(`03 - entEndsWith is set`, (t) => {
+tap.test(`03 - entEndsWith is set`, (t) => {
   t.ok(entEndsWith["1"].p[0] === "sup1");
   t.ok(entEndsWith["1"].p[1] === undefined);
   t.end();
 });
 
-t.test(`04 - entEndsWithCaseInsensitive is set`, (t) => {
-  t.ok(entEndsWithCaseInsensitive["u"].m[0] === "mu");
-  t.ok(entEndsWithCaseInsensitive["u"].m[1] === undefined);
-  t.ok(entEndsWithCaseInsensitive["U"] === undefined);
+tap.test(`04 - entEndsWithCaseInsensitive is set`, (t) => {
+  t.ok(entEndsWithCaseInsensitive.u.m[0] === "mu");
+  t.ok(entEndsWithCaseInsensitive.u.m[1] === undefined);
+  t.ok(entEndsWithCaseInsensitive.U === undefined);
   t.end();
 });
 
-t.test(`05 - decode throws if a non-entity is given`, (t) => {
+tap.test(`05 - decode throws if a non-entity is given`, (t) => {
   t.throws(() => {
     decode("zzz");
   }, /THROW_ID_01/g);
   t.end();
 });
 
-t.test(`06 - decode existing`, (t) => {
+tap.test(`06 - decode existing`, (t) => {
   t.equal(decode("&aleph;"), "\u2135");
   t.end();
 });
 
-t.test(`07 - decode non-existing`, (t) => {
+tap.test(`07 - decode non-existing`, (t) => {
   t.equal(decode("&lsdjhfkhgjd;"), null);
   t.end();
 });
 
-t.test(`08 - decode numeric`, (t) => {
+tap.test(`08 - decode numeric`, (t) => {
   // &#x2135; is &aleph; only numeric version of it
   t.equal(decode("&#x2135;"), null);
   t.end();
 });
 
-t.test(`09 - brokenNamedEntities.json is OK`, (t) => {
+tap.test(`09 - brokenNamedEntities.json is OK`, (t) => {
   t.ok(typeof brokenNamedEntities === "object");
   t.ok(Object.keys(brokenNamedEntities).length > 0);
   Object.keys(brokenNamedEntities).forEach((oneOfEntities, i) => {
@@ -84,24 +84,24 @@ t.test(`09 - brokenNamedEntities.json is OK`, (t) => {
   t.end();
 });
 
-t.test(`10 - minLength is numeric`, (t) => {
+tap.test(`10 - minLength is numeric`, (t) => {
   t.ok(Number.isInteger(minLength));
   t.ok(minLength > 0);
   t.end();
 });
 
-t.test(`11 - maxLength is numeric`, (t) => {
+tap.test(`11 - maxLength is numeric`, (t) => {
   t.ok(Number.isInteger(maxLength));
   t.ok(maxLength > 0);
   t.end();
 });
 
-t.test(`12 - allNamedEntities checks`, (t) => {
+tap.test(`12 - allNamedEntities checks`, (t) => {
   t.ok(Object.keys(allNamedEntities).length > 0);
   t.end();
 });
 
-t.test(`13 - uncertain list is set`, (t) => {
+tap.test(`13 - uncertain list is set`, (t) => {
   t.ok(!!uncertain.Alpha);
   t.ok(!!uncertain.alpha);
   t.ok(!!uncertain.amp);
