c74886afc9d28d8aa968f4b0ea949e21c963b8a9
Roy Revelt <roy@codsen.com>
2020-04-28 05:38:58 +0100

    perf: beef up code sample used in perf tests to cover more cases + wipe historical perf records

--- packages/string-fix-broken-named-entities/perf/check.js
+++ packages/string-fix-broken-named-entities/perf/check.js
@@ -9,7 +9,28 @@ const runPerf = require("../../../scripts/run-perf.js");
 // setup
 const f = require("..");
 
-const testme = () => f("&&NbSpzzz&&NbSpzzz\ny &isindot; z\n&nsp;\n&pound");
+const testme = () =>
+  f(`&&NbSpzzz&&NbSpzzz
+  y &isindot; z
+  &nsp;
+  &pound
+  z &ang y
+  x &ang y&ang z
+  text&angtext&angtext
+  text&pitext&pitext
+  text&pivtext&pivtext
+  text&Pitext&Pitext
+  text&pivtext&pivtext
+  text &ang text&ang text text &ang text&ang text text &ang text&ang text
+  x &Pound2; y
+  &Acd;
+  &#163;
+  &#xA3;
+  one pound;
+  y &nbs; z
+  y &nbp; z
+  &nbs;&nbp;
+`);
 
 // action
 runPerf(testme, callerDir);
