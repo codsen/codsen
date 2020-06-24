0ad700ccc9a55bffe5e9ad7be26600fac4124022
Roy Revelt <roy@codsen.com>
2020-06-24 07:24:26 +0100

    test: add CJS sanity check test

---
+++ packages/html-crush/test/cjs-test.js
@@ -0,0 +1,9 @@
+import tap from "tap";
+import { crush, defaults, version } from "../dist/html-crush.cjs";
+
+tap.test("CJS build works fine", (t) => {
+  t.equal(crush("<div>   <div>").result, "<div> <div>", "01.01");
+  t.match(version, /\d+\.\d+\.\d+/, "01.02");
+  t.ok(Object.keys(defaults).length, "01.03");
+  t.end();
+});
