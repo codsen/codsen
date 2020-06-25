366d04c997249cf07a781241da95f5a78886d882
Roy Revelt <roy@codsen.com>
2020-06-25 04:51:11 +0100

    test: add test for CSS comments within HTML inline styles

--- packages/html-crush/test/removeCSSComments.js
+++ packages/html-crush/test/removeCSSComments.js
@@ -4,22 +4,51 @@ import { crush as m } from "../dist/html-crush.esm";
 tap.test(
   `01 - ${`\u001b[${33}m${`css comments`}\u001b[${39}m`} - in head styles`,
   (t) => {
+    const source = `<style>/* remove this */</style><body>z</body>`;
+
     // off
     t.same(
-      m(`<style>/* remove this */</style><body>z</body>`, {
+      m(source, {
         removeCSSComments: false,
       }).result,
-      `<style>/* remove this */</style><body>z</body>`,
+      source,
       "01.01"
     );
 
     // on
     t.same(
-      m(`<style>/* remove this */</style><body>z</body>`, {
+      m(source, {
         removeCSSComments: true,
       }).result,
       `<style></style><body>z</body>`,
       "01.02"
+    );
+
+    t.end();
+  }
+);
+
+tap.test(
+  `02 - ${`\u001b[${33}m${`css comments`}\u001b[${39}m`} - within body`,
+  (t) => {
+    const source = `<div style="display:block;/*font-size: 1px;*/width:100px;"></div>`;
+
+    // off
+    t.same(
+      m(source, {
+        removeCSSComments: false,
+      }).result,
+      source,
+      "02.01"
+    );
+
+    // on
+    t.same(
+      m(source, {
+        removeCSSComments: true,
+      }).result,
+      `<div style="display:block;width:100px;"></div>`,
+      "02.02"
     );
 
     t.end();
