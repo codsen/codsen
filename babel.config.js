c3df45ae19595b04120359b98a0438f110bc09b3
Roy Revelt <roy@codsen.com>
2020-12-22 22:07:34 +0000

    build: wire up TS setup, split tsconfig into two flavours, beef up babel config

--- babel.config.js
+++ babel.config.js
@@ -1,41 +1,25 @@
-const browserSupport = {
-  browsers: [
-    "Android >= 4.4",
-    "Edge >= 15",
-    "ie 11",
-    "iOS >= 10",
-    "last 2 ChromeAndroid versions",
-    "last 2 Firefox versions",
-    "last 2 FirefoxAndroid versions",
-    "last 2 Opera versions",
-    "Safari >= 9",
-    "last 2 Chrome versions",
-  ],
-};
+const { NODE_ENV } = process.env;
 
-module.exports = function babelConfig(api) {
-  api.cache(true);
-
-  const presets = [
+module.exports = {
+  presets: [
+    "@babel/typescript",
     [
-      "@babel/preset-env",
+      "@babel/env",
       {
-        corejs: 3,
-        targets: browserSupport,
-        useBuiltIns: "entry",
+        targets: {
+          browsers: ["ie >= 11"],
+        },
+        exclude: ["transform-async-to-generator", "transform-regenerator"],
+        modules: false,
+        loose: true,
       },
     ],
-  ];
-
-  const plugins = [
+  ],
+  plugins: [
     "@babel/plugin-proposal-nullish-coalescing-operator",
     "@babel/plugin-proposal-object-rest-spread",
     "@babel/plugin-proposal-optional-chaining",
     "@babel/plugin-proposal-class-properties",
-  ];
-
-  return {
-    presets,
-    plugins,
-  };
+    NODE_ENV === "test" && "@babel/transform-modules-commonjs",
+  ].filter(Boolean),
 };
