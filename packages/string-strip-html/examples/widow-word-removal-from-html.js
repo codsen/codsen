d27967e01362f387482769a5cf24e6aa573c9716
Roy Revelt <roy@codsen.com>
2021-01-20 05:47:10 +0000

    docs: add an example

---
+++ packages/string-strip-html/examples/widow-word-removal-from-html.js
@@ -0,0 +1,25 @@
+// Widow word removal from text within HTML
+
+import { strict as assert } from "assert";
+import { stripHtml } from "../dist/string-strip-html.esm.js";
+import { removeWidows } from "../../string-remove-widows";
+
+const someHtml = `The quick brown fox jumps of the lazy dog.<div class="a">`;
+
+// default widow word removal libs are not aware of HTML:
+// -----------------------------------------------------------------------------
+
+assert.equal(
+  removeWidows(someHtml).res,
+  `The quick brown fox jumps of the lazy dog.<div&nbsp;class="a">` // 😱
+);
+
+// luckily, removeWidows() consumes optional HTML tag locations
+assert.equal(
+  removeWidows(someHtml, {
+    tagRanges: stripHtml(someHtml)
+      // remove the third argument, what to insert ("&nbsp;" string in these cases)
+      .ranges.map(([from, to]) => [from, to]),
+  }).res,
+  `The quick brown fox jumps of the lazy&nbsp;dog.<div class="a">` // ✅
+);
