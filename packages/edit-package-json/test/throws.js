49c578e7c28c6d28a8386b444e683a40e24f42a9
Roy Revelt <roy@codsen.com>
2021-01-25 07:10:20 +0000

    test: assert on throw ID directly

--- packages/edit-package-json/test/throws.js
+++ packages/edit-package-json/test/throws.js
@@ -8,30 +8,25 @@ tap.test(
   `01 - ${`\u001b[${31}m${`api`}\u001b[${39}m`} - ${`\u001b[${33}m${`set`}\u001b[${39}m`} - wrong/missing 1st arg = throw`,
   (t) => {
     // throw test pinning:
-    const error1 = t.throws(() => {
+    t.throws(() => {
       set();
-    });
-    t.match(error1.message, /THROW_ID_01/, "01.01");
+    }, /THROW_ID_01/);
 
-    const error2 = t.throws(() => {
+    t.throws(() => {
       set("");
-    });
-    t.match(error2.message, /THROW_ID_01/, "01.02");
+    }, /THROW_ID_01/);
 
-    const error3 = t.throws(() => {
+    t.throws(() => {
       set(null);
-    });
-    t.match(error3.message, /THROW_ID_01/, "01.03");
+    }, /THROW_ID_01/);
 
-    const error4 = t.throws(() => {
+    t.throws(() => {
       set(undefined);
-    });
-    t.match(error4.message, /THROW_ID_01/, "01.04");
+    }, /THROW_ID_01/);
 
-    const error5 = t.throws(() => {
+    t.throws(() => {
       set(true);
-    });
-    t.match(error5.message, /THROW_ID_01/, "01.05");
+    }, /THROW_ID_01/);
 
     t.end();
   }
@@ -41,30 +36,25 @@ tap.test(
   `02 - ${`\u001b[${31}m${`api`}\u001b[${39}m`} - ${`\u001b[${32}m${`del`}\u001b[${39}m`} - wrong/missing 1st arg = throw`,
   (t) => {
     // throw test pinning:
-    const error1 = t.throws(() => {
+    t.throws(() => {
       del();
-    });
-    t.match(error1.message, /THROW_ID_02/, "02.01");
+    }, /THROW_ID_02/);
 
-    const error2 = t.throws(() => {
+    t.throws(() => {
       del("");
-    });
-    t.match(error2.message, /THROW_ID_02/, "02.02");
+    }, /THROW_ID_02/);
 
-    const error3 = t.throws(() => {
+    t.throws(() => {
       del(null);
-    });
-    t.match(error3.message, /THROW_ID_02/, "02.03");
+    }, /THROW_ID_02/);
 
-    const error4 = t.throws(() => {
+    t.throws(() => {
       del(undefined);
-    });
-    t.match(error4.message, /THROW_ID_02/, "02.04");
+    }, /THROW_ID_02/);
 
-    const error5 = t.throws(() => {
+    t.throws(() => {
       del(true);
-    });
-    t.match(error5.message, /THROW_ID_02/, "02.05");
+    }, /THROW_ID_02/);
 
     t.end();
   }
