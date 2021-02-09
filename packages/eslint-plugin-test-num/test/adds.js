4182728adbb277a07e2d326610da9fdb1b6c0330
Roy Revelt <roy@codsen.com>
2021-02-09 06:55:08 +0000

    test: add tests against TS parser

--- packages/eslint-plugin-test-num/test/adds.js
+++ packages/eslint-plugin-test-num/test/adds.js
@@ -1,5 +1,6 @@
 import tap from "tap";
 import { Linter } from "eslint";
+import * as parser from "@typescript-eslint/parser";
 import api from "../dist/eslint-plugin-test-num.esm";
 import {
   c,
@@ -10,9 +11,49 @@ import {
   // backslash,
 } from "./util/util";
 
-const linter = new Linter();
+function verifyAndFix(t, str, opts) {
+  // ensure that TS parser result is the same
+  const linter = new Linter();
+  // console.log(`linter.version = ${linter.version}`);
+  linter.defineRule("test-num/correct-test-num", api.rules["correct-test-num"]);
 
-linter.defineRule("test-num/correct-test-num", api.rules["correct-test-num"]);
+  const tsLinter = new Linter();
+  tsLinter.defineRule(
+    "test-num/correct-test-num",
+    api.rules["correct-test-num"]
+  );
+  tsLinter.defineParser("@typescript-eslint/parser", parser);
+  t.match(
+    linter.verifyAndFix(str, opts),
+    tsLinter.verifyAndFix(str, opts),
+    "the TS parser output is not the same as native esprima's!"
+  );
+
+  // now just return the output
+  return linter.verifyAndFix(str, opts);
+}
+
+function verify(t, str, opts) {
+  // ensure that TS parser result is the same
+  const linter = new Linter();
+  // console.log(`linter.version = ${linter.version}`);
+  linter.defineRule("test-num/correct-test-num", api.rules["correct-test-num"]);
+
+  const tsLinter = new Linter();
+  tsLinter.defineRule(
+    "test-num/correct-test-num",
+    api.rules["correct-test-num"]
+  );
+  tsLinter.defineParser("@typescript-eslint/parser", parser);
+  t.match(
+    linter.verify(str, opts),
+    tsLinter.verify(str, opts),
+    "the TS parser output is not the same as native esprima's!"
+  );
+
+  // now just return the output
+  return linter.verify(str, opts);
+}
 
 // test, does it add a message argument!!!
 // -----------------------------------------------------------------------------
@@ -21,7 +62,7 @@ tap.test(
   `01 - ${`\u001b[${33}m${`adds the "message" arg`}\u001b[${39}m`} - adds 3rd arg, one liners`,
   (t) => {
     // ensure "in" is fixed
-    const resIn = linter.verifyAndFix(read("07-in"), c);
+    const resIn = verifyAndFix(t, read("07-in"), c);
     t.match(
       resIn,
       {
@@ -32,7 +73,7 @@ tap.test(
     );
 
     // ensure no more errors are raised about "out"
-    const messages = linter.verify(read("07-out"), c);
+    const messages = verify(t, read("07-out"), c);
     t.strictSame(messages, [], `01.02`);
     t.end();
   }
@@ -42,11 +83,11 @@ tap.test(
   `02 - ${`\u001b[${33}m${`adds the "message" arg`}\u001b[${39}m`} - adds 3rd arg, one liners`,
   (t) => {
     // ensure "in" is fixed
-    const resIn = linter.verifyAndFix(read("08-in"), c);
+    const resIn = verifyAndFix(t, read("08-in"), c);
     t.is(resIn.output, read("08-out"), "02.01");
 
     // ensure no more errors are raised about "out"
-    const messages = linter.verify(read("08-out"), c);
+    const messages = verify(t, read("08-out"), c);
     t.strictSame(messages, [], `02.02`);
     t.end();
   }
@@ -56,11 +97,11 @@ tap.test(
   `03 - ${`\u001b[${33}m${`adds the "message" arg`}\u001b[${39}m`} - adds 3rd arg, ends with array value`,
   (t) => {
     // ensure "in" is fixed
-    const resIn = linter.verifyAndFix(read("09-in"), c);
+    const resIn = verifyAndFix(t, read("09-in"), c);
     t.is(resIn.output, read("09-out"), "03.01");
 
     // ensure no more errors are raised about "out"
-    const messages = linter.verify(read("09-out"), c);
+    const messages = verify(t, read("09-out"), c);
     t.strictSame(messages, [], `03.02`);
     t.end();
   }
