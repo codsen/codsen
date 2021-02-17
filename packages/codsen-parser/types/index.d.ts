66d2b2c60cdf88ce734443b9742d774401c0d914
Roy Revelt <roy@codsen.com>
2021-02-17 07:35:32 +0000

    fix: build against the latest tokenizer, with `!important` support

--- packages/codsen-parser/types/index.d.ts
+++ packages/codsen-parser/types/index.d.ts
@@ -14,6 +14,9 @@ interface Property {
     value: string;
     valueStarts: null | number;
     valueEnds: null | number;
+    importantStarts: null | number;
+    importantEnds: null | number;
+    important: null | string;
     semi: null | number;
     start: number;
     end: number;
