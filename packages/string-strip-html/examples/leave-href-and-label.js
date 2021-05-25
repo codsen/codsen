a1d9f88b2d76b7b66a2adf2bb2411ce56770ca27
Roy Revelt <roy@codsen.com>
2021-05-25 06:44:28 +0100

    docs: add another example

---
+++ packages/string-strip-html/examples/leave-href-and-label.js
@@ -0,0 +1,37 @@
+// Retain href and link label
+
+import { strict as assert } from "assert";
+import { stripHtml } from "../dist/string-strip-html.esm.js";
+
+const someHtml = `<a href="https://twitter.com/loretparisi">twitter:loretparisi&nbsp;&oslash;</a>`;
+
+assert.equal(
+  stripHtml(someHtml, {
+    skipHtmlDecoding: true,
+  }).result,
+  `twitter:loretparisi&nbsp;&oslash;`
+);
+
+assert.equal(
+  stripHtml(someHtml, {
+    skipHtmlDecoding: true,
+    cb: ({ tag, deleteFrom, deleteTo, insert, rangesArr, proposedReturn }) => {
+      let temp;
+      if (
+        tag.name === "a" &&
+        tag.attributes &&
+        tag.attributes.some((attr) => {
+          if (attr.name === "href") {
+            temp = attr.value;
+            return true;
+          }
+        })
+      ) {
+        rangesArr.push([deleteFrom, deleteTo, `${temp} ${insert}`]);
+      } else {
+        rangesArr.push(proposedReturn);
+      }
+    },
+  }).result,
+  `https://twitter.com/loretparisi twitter:loretparisi&nbsp;&oslash;`
+);
