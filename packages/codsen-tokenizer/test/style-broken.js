0516ddb25b0004d70809e07536f2f547d94ab731
Roy Revelt <roy@codsen.com>
2021-05-30 21:20:53 +0100

    test: add a new todo test

--- packages/codsen-tokenizer/test/style-broken.js
+++ packages/codsen-tokenizer/test/style-broken.js
@@ -11,85 +11,14 @@ tap.todo(`01`, (t) => {
       gathered.push(obj);
     },
   });
-  t.match(
-    gathered,
-    [
-      {
-        type: "tag",
-        start: 0,
-        end: 6,
-        value: "<style",
-        tagNameStartsAt: 1,
-        tagNameEndsAt: 6,
-        tagName: "style",
-        recognised: true,
-        closing: false,
-        void: false,
-        pureHTML: true,
-        kind: null,
-        attribs: [],
-      },
-      {
-        type: "rule",
-        start: 7,
-        end: 25,
-        value: ".a{b:c !important;}",
-        left: 6,
-        nested: false,
-        openingCurlyAt: 8,
-        closingCurlyAt: 24,
-        selectorsStart: 7,
-        selectorsEnd: 8,
-        selectors: [
-          {
-            value: ".a",
-            selectorStarts: 7,
-            selectorEnds: 9,
-          },
-        ],
-        properties: [
-          {
-            start: 10,
-            end: 25,
-            property: "b",
-            propertyStarts: 9,
-            propertyEnds: 10,
-            value: "c",
-            valueStarts: 11,
-            valueEnds: 12,
-            important: "!important",
-            importantStarts: 13,
-            importantEnds: 23,
-            colon: 10,
-            semi: 23,
-          },
-        ],
-      },
-      {
-        type: "tag",
-        start: 25,
-        end: 33,
-        value: "</style>",
-        tagNameStartsAt: 27,
-        tagNameEndsAt: 32,
-        tagName: "style",
-        recognised: true,
-        closing: true,
-        void: false,
-        pureHTML: true,
-        kind: null,
-        attribs: [],
-      },
-    ],
-    "01"
-  );
+  t.match(gathered, [], "01");
 
   t.end();
 });
 
 tap.todo(`02`, (t) => {
   const gathered = [];
-  ct(`<style\n.a{b:c !important;}</style>`, {
+  ct(`<style.a.a\n.a{b:c !important;}</style.a>`, {
     tagCb: (obj) => {
       gathered.push(obj);
     },
@@ -115,7 +44,7 @@ tap.todo(`03 - standalone semi in head CSS - chopped`, (t) => {
 
 tap.todo(`04 - standalone semi in head CSS - closed`, (t) => {
   const gathered = [];
-  ct(`<style>.red{color:red!important};</style>`, {
+  ct(`<style.a.a.a.a.a>.red{color:red!important};</style.a.a.a.a.a.a.a>`, {
     tagCb: (obj) => {
       gathered.push(obj);
     },
