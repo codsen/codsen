5ff5b3bc46ba754784ed2b5592042e7f38f0be99
Roy Revelt <roy@codsen.com>
2021-11-18 18:56:38 +0000

    build: stop generating testStats

--- scripts/generate-info.js
+++ scripts/generate-info.js
@@ -4,58 +4,39 @@
 
 import fs from "fs";
 import path from "path";
-import { parseTap } from "tap-parse-string-to-object";
 import git from "simple-git";
 import { sortAllObjectsSync } from "../packages/json-comb-core/dist/json-comb-core.esm.js";
-
-const dateObj = new Date();
-const month = dateObj.getUTCMonth() + 1; // months from 1-12
-const day = dateObj.getUTCDate();
-const year = dateObj.getUTCFullYear();
-const newdate = `${year}-${`${month}`.padStart(2, "0")}-${`${day}`.padStart(
-  2,
-  "0"
-)}`;
-
-// FUNCTIONS
-// =========
-
-function median(values) {
-  if (values.length === 0) {
-    return 0;
-  }
-
-  values.sort((a, b) => {
-    return a - b;
-  });
-
-  const half = Math.floor(values.length / 2);
-
-  if (values.length % 2) {
-    return values[half];
-  }
-
-  return (values[half - 1] + values[half]) / 2.0;
-}
 
 // READ ALL LIBS
 // =============
 
-const allPackages = fs
-  .readdirSync(path.resolve("packages"))
-  .filter(
-    (packageName) =>
-      typeof packageName === "string" &&
-      packageName.length &&
-      fs.statSync(path.join("packages", packageName)).isDirectory() &&
-      fs.statSync(path.join("packages", packageName, "package.json")) &&
-      !JSON.parse(
+const allPackages = [];
+const cliPackages = [];
+const programPackages = [];
+
+fs.readdirSync(path.resolve("packages"))
+  .filter((d) => fs.statSync(path.join("packages", d)).isDirectory())
+  .forEach((packageName) => {
+    try {
+      const packageJsonContents = JSON.parse(
         fs.readFileSync(
           path.join("packages", packageName, "package.json"),
           "utf8"
         )
-      ).private
-  );
+      );
+      if (!packageJsonContents.private) {
+        allPackages.push(packageJsonContents.name);
+      }
+      if (packageJsonContents.bin) {
+        cliPackages.push(packageJsonContents.name);
+      }
+      if (packageJsonContents.devDependencies.rollup) {
+        programPackages.push(packageJsonContents.name);
+      }
+    } catch (error) {
+      // nothing happens and we skip it
+    }
+  });
 
 const interdep = [];
 
@@ -82,7 +63,7 @@ for (let i = 0, len = allPackages.length; i < len; i++) {
   const pack = JSON.parse(
     fs.readFileSync(path.join("packages", name, "package.json"))
   );
-  let size;
+  let size = 0;
   if (pack.bin && !pack.lect.special) {
     // cli's
     size = fs.readFileSync(path.join("packages", name, `cli.js`)).length;
@@ -94,8 +75,12 @@ for (let i = 0, len = allPackages.length; i < len; i++) {
         path.join("packages", name, "dist", `${name}.esm.js`)
       ).length;
     } catch (e) {
-      // gulp plugins etc. don't have "dist/*"
-      size = fs.readFileSync(path.join("packages", name, `index.js`)).length;
+      try {
+        // gulp plugins etc. don't have "dist/*"
+        size = fs.readFileSync(path.join("packages", name, `index.js`)).length;
+      } catch (error) {
+        // let's ignore all other unique ad-hoc packages like perf-ref
+      }
     }
   }
 
@@ -106,36 +91,6 @@ for (let i = 0, len = allPackages.length; i < len; i++) {
       ? Object.keys(pack.dependencies).filter((n) => allPackages.includes(n))
       : [],
   });
-
-  // compile test stats
-  try {
-    const obj = { name };
-    fs.accessSync(path.join("packages", name, "testStats.md"), fs.F_OK);
-    const fileContents = fs.createReadStream(
-      path.join("packages", name, "testStats.md")
-    );
-    const res = await parseTap(fileContents);
-    if (typeof res === "object" && res.ok === true) {
-      obj.suites = res.suitesTotal;
-      obj.asserts = res.assertsTotal;
-    }
-    // console.log(
-    //   `121 FINAL ${`\u001b[${33}m${`obj`}\u001b[${39}m`} = ${JSON.stringify(
-    //     obj,
-    //     null,
-    //     4
-    //   )}`
-    // );
-    allStats.push({ ...obj });
-  } catch (e) {
-    console.log(
-      `! couldn't read/parse ${path.join(
-        "packages",
-        name,
-        "testStats.md"
-      )}\n${e}`
-    );
-  }
 
   // compile dependency stats
   if (Object.prototype.hasOwnProperty.call(pack, "dependencies")) {
@@ -171,76 +126,24 @@ for (let i = 0, len = allPackages.length; i < len; i++) {
   }
 }
 
-const compiledAssertionCounts = {
-  all: {},
+const packages = {
+  all: allPackages,
+  cli: cliPackages,
+  programs: programPackages,
   totalPackageCount: allPackages.length,
-  totalAssertsCount: 0,
-  minAssertsCount: 999999999999,
-  minAssertsName: "",
-  maxAssertsCount: 0,
-  maxAssertsName: "",
-  assertsCountMedian: 0,
-  assertsCountArithmeticMean: 0,
-  assertsCountGeometricMean: 0,
+  cliCount: cliPackages.length,
+  programsCount: programPackages.length,
 };
 
 allStats.forEach((statsObj) => {
-  // console.log(
-  //   `${`\u001b[${33}m${`statsObj`}\u001b[${39}m`} = ${JSON.stringify(
-  //     statsObj,
-  //     null,
-  //     4
-  //   )}`
-  // );
-  if (
-    typeof statsObj === "object" &&
-    statsObj !== null &&
-    statsObj.asserts &&
-    Number.isInteger(statsObj.asserts)
-  ) {
-    compiledAssertionCounts.totalAssertsCount += statsObj.asserts;
-    if (statsObj.asserts > compiledAssertionCounts.maxAssertsCount) {
-      compiledAssertionCounts.maxAssertsCount = statsObj.asserts;
-      compiledAssertionCounts.maxAssertsName = statsObj.name;
-    }
-    if (
-      statsObj.asserts < compiledAssertionCounts.minAssertsCount &&
-      statsObj.asserts > 1
-    ) {
-      compiledAssertionCounts.minAssertsCount = statsObj.asserts;
-      compiledAssertionCounts.minAssertsName = statsObj.name;
-    }
-  }
+  console.log(
+    `${`\u001b[${33}m${`statsObj`}\u001b[${39}m`} = ${JSON.stringify(
+      statsObj,
+      null,
+      4
+    )}`
+  );
 });
-
-compiledAssertionCounts.assertsCountArithmeticMean = Math.round(
-  compiledAssertionCounts.totalAssertsCount / allPackages.length
-);
-
-compiledAssertionCounts.assertsCountGeometricMean = Math.round(
-  Math.pow(
-    allStats.reduce((accum, curr) => curr.asserts * accum, 1),
-    1 / allStats.length
-  )
-);
-
-allStats.forEach((statsObj) => {
-  compiledAssertionCounts.all[statsObj.name] = statsObj.asserts;
-});
-
-compiledAssertionCounts.assertsCountMedian = median(
-  Object.keys(compiledAssertionCounts.all).map(
-    (packageName) => compiledAssertionCounts.all[packageName]
-  )
-);
-
-// console.log(
-//   `242 generate-info.js: ${`\u001b[${33}m${`compiledAssertionCounts`}\u001b[${39}m`} = ${JSON.stringify(
-//     compiledAssertionCounts,
-//     null,
-//     4
-//   )}`
-// );
 
 // 3. compile top 10 of own and external deps and devdeps
 // -----------------------------------------------------------------------------
@@ -322,15 +225,13 @@ fs.writeFile(
 );
 
 fs.writeFile(
-  path.resolve("stats/compiledAssertionCounts.json"),
-  JSON.stringify(compiledAssertionCounts, null, 4),
+  path.resolve("stats/packages.json"),
+  JSON.stringify(packages, null, 4),
   (err) => {
     if (err) {
       throw err;
     }
-    console.log(
-      `\u001b[${32}m${`compiledAssertionCounts.json written OK`}\u001b[${39}m`
-    );
+    console.log(`\u001b[${32}m${`packages.json written OK`}\u001b[${39}m`);
   }
 );
 
@@ -347,62 +248,7 @@ fs.writeFile(
   }
 );
 
-// 5. assemble historical unit test totals
-// -----------------------------------------------------------------------------
-
-let oldHistoricTotals;
-try {
-  oldHistoricTotals = JSON.parse(
-    fs.readFileSync(path.join("stats/oldHistoricTotals.json"))
-  );
-} catch (e) {
-  console.log(
-    `error white reading/parsing stats/oldHistoricTotals.json!\n${e}`
-  );
-}
-
-// oldHistoricTotals is array of arrays: [date, total]
-// if the last value is not the same, push
-if (
-  !oldHistoricTotals.length ||
-  oldHistoricTotals[oldHistoricTotals.length - 1][1] !==
-    compiledAssertionCounts.totalAssertsCount
-) {
-  let findingIdx;
-  if (
-    oldHistoricTotals.some((arr, i) => {
-      if (arr[0] === newdate) {
-        findingIdx = i;
-        return true;
-      }
-      return false;
-    })
-  ) {
-    // edit old if it was on the same date, today
-    oldHistoricTotals[findingIdx][1] =
-      compiledAssertionCounts.totalAssertsCount;
-  } else {
-    // push new
-    oldHistoricTotals.push([
-      newdate,
-      compiledAssertionCounts.totalAssertsCount,
-    ]);
-  }
-  fs.writeFile(
-    path.join("stats/oldHistoricTotals.json"),
-    JSON.stringify(oldHistoricTotals, null, 4),
-    (err) => {
-      if (err) {
-        throw err;
-      }
-      console.log(
-        `\u001b[${32}m${`oldHistoricTotals.json written OK`}\u001b[${39}m`
-      );
-    }
-  );
-}
-
-// 6. gather git repo info
+// 5. gather git repo info
 // ---------------------------------------------------------------------------
 
 let commitTotal = null;
