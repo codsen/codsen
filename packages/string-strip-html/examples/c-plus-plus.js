bb902af5aa9d64dddffff8dba1680ac618ccd12a
Roy Revelt <roy@codsen.com>
2021-11-22 21:00:47 +0000

    docs: add an example showing how to ignore tag pairs and their contents

---
+++ packages/string-strip-html/examples/c-plus-plus.js
@@ -0,0 +1,18 @@
+// Ignores code tags and their contents
+
+import { strict as assert } from "assert";
+import { stripHtml } from "../dist/string-strip-html.esm.js";
+
+const someHtml = `<code>#include <stdio.h>;</code> and <code>#include &lt;stdio.h&gt;</code>`;
+
+// default behaviour:
+assert.equal(stripHtml(someHtml).result, `#include; and #include`);
+
+// ignore <code> tag pairs
+assert.equal(
+  stripHtml(someHtml, {
+    ignoreTagsWithTheirContents: ["code"],
+    skipHtmlDecoding: true,
+  }).result,
+  someHtml
+);
