5f8b4bae2e6f5ce00006a92a3973a6a5a4962ffe
Roy Revelt <roy@codsen.com>
2021-12-24 19:40:27 +0000

    docs: update root readme generator and build the latest readme itself

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # Codsen
 
-> A lerna monorepo for our 118 npm packages 📦📦📦
+> A turbo-monorepo of 118 npm packages 📦📦📦
 
 ## 📚 Documentation
 
 Please [visit codsen.com](https://codsen.com/os/) for an overview and full documentation of all packages.
 
+## 🛠️ Tech stack
+
+- `yarn` — with workspaces
+- `lerna` — for version bumping and changelogs
+- `turborepo` — to run tasks
+- `uvu` + `c8` — unit test runner and coverage
+- `typescript` — for all source code
+- `esbuild` — to build `*.ts` into ESM and IIFE
+- `rollup` + `rollup-plugin-dts` — to generate `*.d.ts`
+
 ## 🐛 Issue Tracker
 
-For bugs, feature requests and so on use the [Issue Tracker](https://github.com/codsen/codsen/issues/new/choose).
+For bugs, feature requests and so on, use the [Issue Tracker](https://github.com/codsen/codsen/issues/new/choose).
 
 ## 💼 Licence
 
--- ops/scripts/generate-readme.js
+++ ops/scripts/generate-readme.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+// ==================================
+// GENERATES THE MONOREPO ROOT README
+// ==================================
+
 import fs from "fs";
 import path from "path";
 
@@ -30,15 +34,25 @@ const allPackages = fs
 
 const template = `# Codsen
 
-> A lerna monorepo for our ${allPackages.length} npm packages 📦📦📦
+> A turbo-monorepo of ${allPackages.length} npm packages 📦📦📦
 
 ## 📚 Documentation
 
 Please [visit codsen.com](https://codsen.com/os/) for an overview and full documentation of all packages.
 
+## 🛠️ Tech stack
+
+- \`yarn\` — with workspaces
+- \`lerna\` — for version bumping and changelogs
+- \`turborepo\` — to run tasks
+- \`uvu\` + \`c8\` — unit test runner and coverage
+- \`typescript\` — for all source code
+- \`esbuild\` — to build \`*.ts\` into ESM and IIFE
+- \`rollup\` + \`rollup-plugin-dts\` — to generate \`*.d.ts\`
+
 ## 🐛 Issue Tracker
 
-For bugs, feature requests and so on use the [Issue Tracker](https://github.com/codsen/codsen/issues/new/choose).
+For bugs, feature requests and so on, use the [Issue Tracker](https://github.com/codsen/codsen/issues/new/choose).
 
 ## 💼 Licence
 
