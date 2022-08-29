e0152a740621daab36e346672be5114c6f5a09c7
Roy Revelt <roy@codsen.com>
2022-08-29 18:44:01 +0000

    chore: automated build tasks [skip ci]

--- packages/seo-editor/types/index.d.ts
+++ packages/seo-editor/types/index.d.ts
@@ -16,10 +16,20 @@ interface Res {
     timeTakenInMilliseconds: number;
   };
 }
+declare function isLetter(str: unknown): boolean;
+declare function setLengthCompensation(
+  resultArr: Res["result"],
+  receivedMaxLen: number
+): {
+  extracted: string;
+  counts: number[];
+  length: number;
+  lengthCompensation: number;
+}[];
 declare function editor(
   todo?: string,
   copy?: string,
   opts?: Partial<Opts>
 ): Res;
 
-export { Opts, editor, version };
+export { Opts, Res, editor, isLetter, setLengthCompensation, version };
