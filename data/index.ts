f8a7e23e308d954be1dd129f9330cc8b5cc5bef4
Roy Revelt <roy@codsen.com>
2022-10-23 14:38:25 +0100

    build: start exporting the changelogs from data

--- data/index.ts
+++ data/index.ts
@@ -2,6 +2,7 @@ export * from "./sources/programClassification.js";
 export * from "./sources/exportedDefaults.js";
 export * from "./sources/dependencyStats.js";
 export * from "./sources/packageJSONData.js";
+export * from "./sources/changelogs.js";
 export * from "./sources/interdeps.js";
 export * from "./sources/gitStats.js";
 export * from "./sources/examples.js";
