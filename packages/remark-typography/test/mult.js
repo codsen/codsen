1e3c9c33455307f60542d2d3612e11e62aa2db97
Roy Revelt <roy@codsen.com>
2022-12-27 14:05:19 +0000

    test: add an upcoming feature test

--- packages/remark-typography/test/mult.js
+++ packages/remark-typography/test/mult.js
@@ -1,21 +1,22 @@
 import { test } from "uvu";
 // eslint-disable-next-line no-unused-vars
 import { equal, is, ok, throws, type, not, match } from "uvu/assert";
-import { remark } from "remark";
-import { multiplicationSign, rawNbsp } from "codsen-utils";
+// import { remark } from "remark";
+// import { multiplicationSign, rawNbsp, rawNDash } from "codsen-utils";
 
-import fixTypography from "../dist/remark-typography.esm.js";
+// import fixTypography from "../dist/remark-typography.esm.js";
 
 // -----------------------------------------------------------------------------
 
-test(`01`, async () => {
+// TODO - extract and evaluate whole chunks around dashes, not just the first character
+/* test(`01`, async () => {
   equal(
-    (await remark().use(fixTypography, {}).process("10px x 20px x 30px"))
+    (await remark().use(fixTypography, {}).process("10px x 20px x 30px - 40px"))
       .toString()
       .trim(),
-    `10px ${multiplicationSign} 20px ${multiplicationSign}${rawNbsp}30px`,
+    `10px ${multiplicationSign} 20px ${multiplicationSign} 30px${rawNbsp}${rawNDash}${rawNbsp}40px`,
     "01.01"
   );
-});
+}); */
 
 test.run();
