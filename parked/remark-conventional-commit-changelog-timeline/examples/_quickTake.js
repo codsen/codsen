917a4c8bbcf05ec02436379e89ea4ef4fe619bb4
Roy Revelt <roy@codsen.com>
2023-02-04 08:54:17 +0000

    build: park the remark timeline package for now

---
+++ parked/remark-conventional-commit-changelog-timeline/examples/_quickTake.js
@@ -0,0 +1,60 @@
+// Quick Take
+
+import { strict as assert } from "assert";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import remarkGfm from "remark-gfm";
+import rehypeFormat from "rehype-format";
+import rehypeStringify from "rehype-stringify";
+
+import c from "../dist/remark-conventional-commit-changelog-timeline.esm.js";
+
+function render(str, opts) {
+  let res = unified()
+    .data("settings", { fragment: true })
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(c, opts)
+    .use(rehypeFormat)
+    .use(rehypeStringify)
+    .processSync(str);
+
+  return res.value;
+}
+
+let input = `
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+# 3.1.0 (2022-08-12)
+
+### Features
+
+- abc
+- xyz
+`;
+
+let expected = `
+<h2>3.1.0</h2>
+<div class="release-date">Aug 12, <span>2022</span></div>
+<h3><span class="emoji">✨</span> Features</h3>
+<ul>
+  <li>abc</li>
+  <li>xyz</li>
+</ul>
+`;
+
+assert.equal(
+  render(input, {
+    // defaults:
+    dateDivLocale: "en-US",
+    // eslint-disable-next-line no-unused-vars
+    dateDivMarkup: ({ date, year, month, day }) =>
+      `${month} ${day}, <span>${year}</span>`,
+  }),
+  expected
+);
---
+++ parked/remark-conventional-commit-changelog-timeline/test/cjs.js
@@ -0,0 +1,32 @@
+/* eslint-disable node/no-extraneous-require */
+/* eslint-disable no-prototype-builtins */
+import fs from "fs";
+import path from "path";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+
+const { test } = require("uvu");
+// eslint-disable-next-line no-unused-vars
+const { equal, is, ok, throws, type, not, match } = require("uvu/assert");
+// the CJS imported file can't be ".cjs.js", so we rename temporarily:
+fs.renameSync(
+  path.resolve("dist/remark-conventional-commit-changelog-timeline.cjs.js"),
+  path.resolve("dist/remark-conventional-commit-changelog-timeline.cjs")
+);
+const api = require("../dist/remark-conventional-commit-changelog-timeline.cjs");
+
+test.after(() => {
+  fs.renameSync(
+    path.resolve("dist/remark-conventional-commit-changelog-timeline.cjs"),
+    path.resolve("dist/remark-conventional-commit-changelog-timeline.cjs.js")
+  );
+});
+
+// -----------------------------------------------------------------------------
+
+test(`01 - a function is exported`, () => {
+  equal(typeof api, "function", "01.01");
+});
+
+test.run();
