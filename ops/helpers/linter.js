252bf975ce3385446eca039596d03db88e7f2c3d
Roy Revelt <roy@codsen.com>
2023-09-11 19:25:06 +0100

    build: improve the testing script

--- ops/helpers/linter.js
+++ ops/helpers/linter.js
@@ -2,10 +2,10 @@ import { Linter } from "eslint";
 import { equal } from "uvu/assert";
 import * as parser from "@typescript-eslint/parser";
 
-export function lint(method, str, config) {
+export function lint(method, str, config, skipParserComparison = false) {
   if (typeof str !== "string") {
     throw new TypeError(
-      "ops/helpers/linter.js/main(): first arg must be string!"
+      "ops/helpers/linter.js/main(): first arg must be string!",
     );
   }
   let { rules, plugins } = config;
@@ -14,53 +14,79 @@ export function lint(method, str, config) {
   });
 
   // ensure that TS parser's result is the same
-  if (method === "verifyAndFix") {
+  if (!skipParserComparison && method === "verifyAndFix") {
     equal(
-      linter.verifyAndFix(str, {
+      linter.verifyAndFix(
+        str,
+        {
+          plugins,
+          rules,
+          languageOptions: {
+            parser,
+          },
+        },
+        { filename: "foo.js" },
+      ),
+      linter.verifyAndFix(
+        str,
+        {
+          plugins,
+          rules,
+        },
+        { filename: "foo.js" },
+      ),
+      `${method}(): the output of ESLint on TS parser is not the same as native esprima's!`,
+    );
+
+    // now just return the output
+    return linter.verifyAndFix(
+      str,
+      {
         plugins,
         rules,
         languageOptions: {
           parser,
         },
-      }),
-      linter.verifyAndFix(str, {
-        plugins,
-        rules,
-      }),
-      `${method}(): the output of ESLint on TS parser is not the same as native esprima's!`
-    );
-
-    // now just return the output
-    return linter.verifyAndFix(str, {
-      plugins,
-      rules,
-      languageOptions: {
-        parser,
       },
-    });
+      { filename: "foo.js" },
+    );
   } else {
-    equal(
-      linter.verify(str, {
+    if (!skipParserComparison) {
+      equal(
+        linter.verify(
+          str,
+          {
+            plugins,
+            rules,
+            languageOptions: {
+              parser,
+            },
+          },
+          { filename: "foo.js" },
+        ).output,
+        linter.verify(
+          str,
+          {
+            plugins,
+            rules,
+          },
+          { filename: "foo.js" },
+        ).output,
+        `${method}(): the output of ESLint on TS parser is not the same as native esprima's!`,
+      );
+    }
+
+    // now just return the output
+    return linter.verify(
+      str,
+      {
         plugins,
         rules,
         languageOptions: {
           parser,
         },
-      }).output,
-      linter.verify(str, {
-        plugins,
-        rules,
-      }).output,
-      `${method}(): the output of ESLint on TS parser is not the same as native esprima's!`
-    );
-
-    // now just return the output
-    return linter.verify(str, {
-      plugins,
-      rules,
-      languageOptions: {
-        parser,
       },
-    });
+      { filename: "foo.js" },
+    );
   }
 }
