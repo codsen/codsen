f2d85468ebab75281cbe6e8fe45e4ad0ebe57dac
Roy Revelt <roy@codsen.com>
2025-10-08 23:20:15 +0100

    chore: fix perf script, reset historical records

--- packages/codsen-utils/perf/check.js
+++ packages/codsen-utils/perf/check.js
@@ -2,11 +2,11 @@
 import path from "path";
 
 import { runPerf } from "../../../ops/scripts/perf.js";
-import { isNumber } from "../dist/codsen-utils.esm.js";
+import { isPlainObject } from "../dist/codsen-utils.esm.js";
 
 const callerDir = path.resolve(".");
 
-const testme = () => isNumber("The quick brown fox jumps over a lazy dog.");
+const testme = () => isPlainObject({ a: 1 });
 
 // action
 runPerf(testme, callerDir);
