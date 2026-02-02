4bf5702ac1d3a923bf5e1db94bf4ac58e02d9c10
Roy Revelt <roy@codsen.com>
2026-02-02 06:43:56 +0000

    chore: cleanup

--- packages/array-includes-with-glob/perf/check.js
+++ packages/array-includes-with-glob/perf/check.js
@@ -1,5 +1,5 @@
 // deps
-import path from "path";
+import path from "node:path";
 
 import { runPerf } from "../../../ops/scripts/perf.js";
 import { includesWithGlob } from "../dist/array-includes-with-glob.esm.js";
@@ -7,13 +7,9 @@ import { includesWithGlob } from "../dist/array-includes-with-glob.esm.js";
 const callerDir = path.resolve(".");
 
 const testme = () =>
-  includesWithGlob(
-    ["something", "anything", "everything"],
-    ["anything", "zzz"],
-    {
-      arrayVsArrayAllMustBeFound: "any",
-    },
-  );
+  includesWithGlob(["something", "anything", "everything"], ["some*", "zzz"], {
+    arrayVsArrayAllMustBeFound: "any",
+  });
 
 // action
 runPerf(testme, callerDir);
