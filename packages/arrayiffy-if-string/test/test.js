92b981ad1135c1f3f69d3998846a829b43120abe
Roy Revelt <roy@codsen.com>
2026-02-06 05:48:46 +0000

    test: update the test numbering

--- packages/arrayiffy-if-string/test/test.js
+++ packages/arrayiffy-if-string/test/test.js
@@ -1,6 +1,6 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { arrayiffy as a } from "../dist/arrayiffy-if-string.esm.js";
 
