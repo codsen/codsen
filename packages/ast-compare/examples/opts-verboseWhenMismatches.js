cadb87701446ec1ece96a30d1bf6bf93cba8ceaf
Roy Revelt <roy@codsen.com>
2026-02-07 20:50:34 +0000

    docs: update example

--- packages/ast-compare/examples/opts-verboseWhenMismatches.js
+++ packages/ast-compare/examples/opts-verboseWhenMismatches.js
@@ -1,6 +1,6 @@
 // `opts.verboseWhenMismatches`
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { compare } from "../dist/ast-compare.esm.js";
 
