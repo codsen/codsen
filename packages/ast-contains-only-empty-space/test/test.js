8179162f8413cf5fbbdc1592c1dc3f0c8f7cfa82
Roy Revelt <roy@codsen.com>
2026-02-10 05:45:06 +0000

    test: update the test numbering

--- packages/ast-contains-only-empty-space/test/test.js
+++ packages/ast-contains-only-empty-space/test/test.js
@@ -1,6 +1,6 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { empty } from "../dist/ast-contains-only-empty-space.esm.js";
 
