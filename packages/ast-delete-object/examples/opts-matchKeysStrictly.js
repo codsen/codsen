84ac04e3bf69d0c15592d111f1573681eccc6801
Roy Revelt <roy@codsen.com>
2026-02-12 05:19:50 +0000

    docs: update opts match keys strictly example

--- packages/ast-delete-object/examples/opts-matchKeysStrictly.js
+++ packages/ast-delete-object/examples/opts-matchKeysStrictly.js
@@ -1,6 +1,6 @@
 // `opts.matchKeysStrictly`
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { deleteObj } from "../dist/ast-delete-object.esm.js";
 
