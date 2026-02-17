2b81aa100240a8d823cf95315ac1a85bde966042
Roy Revelt <roy@codsen.com>
2026-02-17 05:19:55 +0000

    test: update the test numbering

--- packages/ast-is-empty/test/test.js
+++ packages/ast-is-empty/test/test.js
@@ -1,6 +1,6 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { isEmpty } from "../dist/ast-is-empty.esm.js";
 
@@ -109,7 +109,7 @@ test("12 - string - false", () => {
 });
 
 // ==============================
-// Retruns Null
+// Returns Null
 // ==============================
 
 test("13 - function as input - returns null", () => {
