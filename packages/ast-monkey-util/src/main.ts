3cc0855069588746b437cd41ad9cb6c5b108ca0f
Roy Revelt <roy@codsen.com>
2026-02-25 05:12:26 +0000

    chore: tidy source

--- packages/ast-monkey-util/src/main.ts
+++ packages/ast-monkey-util/src/main.ts
@@ -1,11 +1,11 @@
+import { version as v } from "../package.json";
+import { parentItem } from "./util/parent";
 import { pathNext } from "./util/pathNext";
 import { pathPrev } from "./util/pathPrev";
 import { pathUp } from "./util/pathUp";
-import { parentItem } from "./util/parent";
-import { version as v } from "../package.json";
 
 const version: string = v;
 
 // -----------------------------------------------------------------------------
 
-export { pathNext, pathPrev, pathUp, parentItem as parent, version };
+export { parentItem as parent, pathNext, pathPrev, pathUp, version };
