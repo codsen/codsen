1ececf36b98168b378ab15c09202cb063a362b61
Roy Revelt <roy@codsen.com>
2026-03-06 05:27:28 +0000

    docs: modernise the quick-take example

--- packages/color-shorthand-hex-to-six-digit/examples/_quickTake.js
+++ packages/color-shorthand-hex-to-six-digit/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { conv } from "../dist/color-shorthand-hex-to-six-digit.esm.js";
 
