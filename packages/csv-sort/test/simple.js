51a7f4e8c5db25998d69c555ff74daf76041723a
Roy Revelt <roy@codsen.com>
2026-03-07 05:44:33 +0000

    tests: update simple

--- packages/csv-sort/test/simple.js
+++ packages/csv-sort/test/simple.js
@@ -1,22 +1,22 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import compare from "./util.js";
 
 // Simple file, concentrate on row sorting, Balance, Credit & Debit col detection
 // -------------------------------------------------------------------
 
-test("01. sorts a basic file, empty extra column in header", () => {
-  compare(equal, "simples");
+test("01 - sorts a basic file, empty extra column in header", () => {
+  compare(equal, "simples", "01");
 });
 
-test("02. sorts a basic file, no headers", () => {
-  compare(equal, "simples-no-header");
+test("02 - sorts a basic file, no headers", () => {
+  compare(equal, "simples-no-header", "02");
 });
 
-test("03. sorts a basic file with opposite order of the CSV entries", () => {
-  compare(equal, "simples-backwards");
+test("03 - sorts a basic file with opposite order of the CSV entries", () => {
+  compare(equal, "simples-backwards", "03");
 });
 
 test.run();
