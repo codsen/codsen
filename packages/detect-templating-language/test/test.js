79e8117e47d212bf2cbfb6b08f144fc4b41b5bd2
Roy Revelt <roy@codsen.com>
2026-03-15 04:25:59 +0000

    test: update the test numbering

--- packages/detect-templating-language/test/test.js
+++ packages/detect-templating-language/test/test.js
@@ -1,6 +1,7 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
+/** biome-ignore-all lint/suspicious/noTemplateCurlyInString: fixtures contain literal template syntax under test */
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { detectLang } from "../dist/detect-templating-language.esm.js";
 
