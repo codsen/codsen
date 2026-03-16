d75fb7615ecb268db4d771fef3d53c8e04579f16
Roy Revelt <roy@codsen.com>
2026-03-16 05:27:35 +0000

    tests: update tests

--- packages/detergent/test/mjml.js
+++ packages/detergent/test/mjml.js
@@ -1,19 +1,19 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 // import { det as det1 } from "../dist/detergent.esm.js";
 import { det, mixer } from "../t-util/util.js";
 
 // https://github.com/codsen/codsen/issues/17
-test("01", () => {
+test("001", () => {
   mixer({
     stripHtml: false,
   }).forEach((opt, n) => {
     equal(
       det(ok, not, n, 'a <mj-image src="foo.jpg"> b', opt).res,
       'a <mj-image src="foo.jpg"> b',
-      JSON.stringify(opt, null, 4),
+      `001.01 - ${JSON.stringify(opt, null, 4)}`,
     );
   });
   mixer({
@@ -22,19 +22,19 @@ test("01", () => {
     equal(
       det(ok, not, n, 'a <mj-image src="foo.jpg"> b', opt).res,
       "a b",
-      JSON.stringify(opt, null, 4),
+      `001.02 - ${JSON.stringify(opt, null, 4)}`,
     );
   });
 });
 
-test("02", () => {
+test("002", () => {
   mixer({
     stripHtml: false,
   }).forEach((opt, n) => {
     equal(
       det(ok, not, n, 'a <mj-image src="foo.jpg"></mj-image> b', opt).res,
       'a <mj-image src="foo.jpg"></mj-image> b',
-      JSON.stringify(opt, null, 4),
+      `002.01 - ${JSON.stringify(opt, null, 4)}`,
     );
   });
   mixer({
@@ -43,7 +43,7 @@ test("02", () => {
     equal(
       det(ok, not, n, 'a <mj-image src="foo.jpg"></mj-image> b', opt).res,
       "a b",
-      JSON.stringify(opt, null, 4),
+      `002.02 - ${JSON.stringify(opt, null, 4)}`,
     );
   });
 });
