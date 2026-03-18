be7e84f205434a667175e2a96757700c14ee0dc8
Roy Revelt <roy@codsen.com>
2026-03-18 05:13:18 +0000

    test: update del test numbering

--- packages/edit-package-json/test/del.js
+++ packages/edit-package-json/test/del.js
@@ -1,8 +1,9 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { deleter } from "./util/util.js";
+
 // import { set, del } from "../dist/edit-package-json.esm.js";
 
 // del - delete existing key
