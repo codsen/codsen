9bae0251ee688752ece2b1d6accf147ab7c8a0a9
Roy Revelt <roy@codsen.com>
2026-03-18 05:12:18 +0000

    test: update the casciaro test numbering

--- packages/edit-package-json/test/casciaro.js
+++ packages/edit-package-json/test/casciaro.js
@@ -1,8 +1,9 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { setter } from "./util/util.js";
+
 // import { set, del } from "../dist/edit-package-json.esm.js";
 
 // -----------------------------------------------------------------------------
@@ -21,7 +22,7 @@ const testObj = `{
   }
 }`;
 
-test(`01 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set the value using unicode key - value is number`, () => {
+test(`01 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set the value using unicode key - value is number`, () => {
   let source = `{
   "15\u00f8C": {
     "3\u0111": 1
@@ -35,7 +36,7 @@ test(`01 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, source, result, "15\u00f8C.3\u0111", 2, "04.01");
 });
 
-test(`02 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set the value using unicode key - value is string`, () => {
+test(`02 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set the value using unicode key - value is string`, () => {
   let source = `{
   "15\u00f8C": {
     "3\u0111": "1"
@@ -49,7 +50,7 @@ test(`02 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, source, result, "15\u00f8C.3\u0111", "2", "04.02");
 });
 
-test(`03 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set the value using dot in key`, () => {
+test(`03 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set the value using dot in key`, () => {
   let source = `{
   "a.b": {
     "looks.like": 1
@@ -63,7 +64,7 @@ test(`03 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, source, result, ["a.b", "looks.like"], 2, "04.03");
 });
 
-test(`04 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under shallow object`, () => {
+test(`04 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under shallow object`, () => {
   let input = `{
   "b": {
     "d": ["a"]
@@ -79,7 +80,7 @@ test(`04 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, input, result, "j", { k: "x" }, "04.04");
 });
 
-test(`05 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under shallow object`, () => {
+test(`05 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under shallow object`, () => {
   let input = `{
   "a": "b",
   "b": {
@@ -103,7 +104,7 @@ test(`05 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, input, result, "j", { k: "x" }, "04.05");
 });
 
-test(`06 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under shallow object`, () => {
+test(`06 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under shallow object`, () => {
   let input = `{
   "a": "b",
   "b": {
@@ -129,7 +130,7 @@ test(`06 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
 });
 
 // TODO
-test(`07 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value using number path`, () => {
+test(`07 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value using number path`, () => {
   let source = `{
   "a": "b",
   "b": {
@@ -150,7 +151,7 @@ test(`07 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, source, result, "b.d.0", "x", "04.07");
 });
 
-test(`08 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value using number path`, () => {
+test(`08 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value using number path`, () => {
   let result = `{
   "a": "b",
   "b": {
@@ -163,12 +164,12 @@ test(`08 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, testObj, result, "b.d.0", "x", "04.08");
 });
 
-test(`09 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value using number path`, () => {
+test(`09 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value using number path`, () => {
   // crop of test "should set value using number path", obj.b.d
   setter(equal, '["a", "b"]', '["x", "b"]', 0, "x", "04.09");
 });
 
-test(`10 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under deep object`, () => {
+test(`10 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under deep object`, () => {
   let result = `{
   "a": "b",
   "b": {
@@ -181,7 +182,7 @@ test(`10 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, testObj, result, "b.c", "o", "04.10");
 });
 
-test(`11 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under deep object`, () => {
+test(`11 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under deep object`, () => {
   let result = `{
   "a": "b",
   "b": {
@@ -195,7 +196,7 @@ test(`11 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, testObj, result, "b.c", "o", "04.11");
 });
 
-test(`12 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under deep object`, () => {
+test(`12 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under deep object`, () => {
   let result = `{
   "a": "b",
   "b": {
@@ -208,7 +209,7 @@ test(`12 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, testObj, result, ["b", "c"], "o", "04.12");
 });
 
-test(`13 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under deep object`, () => {
+test(`13 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under deep object`, () => {
   let result = `{
   "a": "b",
   "b": {
@@ -222,7 +223,7 @@ test(`13 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, testObj, result, ["b", "c"], "o", "04.13");
 });
 
-test(`14 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under array`, () => {
+test(`14 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under array`, () => {
   let result = `{
   "a": "b",
   "b": {
@@ -238,7 +239,7 @@ test(`14 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, testObj, result, "b.e.1.f", null, "04.14");
 });
 
-test(`15 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under array`, () => {
+test(`15 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - should set value under array`, () => {
   let result = `{
   "a": "b",
   "b": {
@@ -254,7 +255,7 @@ test(`15 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, testObj, result, ["b", "e", 1, "f"], null, "04.15");
 });
 
-test(`16 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - minimal case, arrays 1`, () => {
+test(`16 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - minimal case, arrays 1`, () => {
   let source = `{
   "a": [{}, { "b": "c" }]
 }`;
@@ -264,7 +265,7 @@ test(`16 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, source, result, "a.1.b", null, "04.16");
 });
 
-test(`17 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - minimal case, arrays 2`, () => {
+test(`17 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - minimal case, arrays 2`, () => {
   let source = `{
   "a": [{ "b": "c" }]
 }`;
@@ -274,7 +275,7 @@ test(`17 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, source, result, "a.0.b", null, "04.17");
 });
 
-test(`18 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - square bracket as value`, () => {
+test(`18 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - square bracket as value`, () => {
   let source = `{
   "a": "[",
   "k": {
@@ -292,7 +293,7 @@ test(`18 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, source, result, "k.no", "9", "04.18");
 });
 
-test(`19 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - curly bracket as value`, () => {
+test(`19 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - curly bracket as value`, () => {
   let source = `{
   "a": "{",
   "k": {
@@ -310,7 +311,7 @@ test(`19 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-p
   setter(equal, source, result, "k.no", "9", "04.19");
 });
 
-test(`20 - ${`\u001b[${36}m${"set"}\u001b[${39}m`} - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - curly bracket as value`, () => {
+test(`20 - set - ${`\u001b[${35}m${"object-path/set()"}\u001b[${39}m`} - curly bracket as value`, () => {
   let source = '{"a": {},"gh": {"mn": "1","yz": "-"}}';
   let result = '{"a": {},"gh": {"mn": "1","yz": "x"}}';
   setter(equal, source, result, "gh.yz", "x", "04.20");
