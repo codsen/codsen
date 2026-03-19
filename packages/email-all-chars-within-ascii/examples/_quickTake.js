39a767e84667365f08596e150e653acae77a4c34
Roy Revelt <roy@codsen.com>
2026-03-19 05:27:40 +0000

    docs: modernise the quick-take example

--- packages/email-all-chars-within-ascii/examples/_quickTake.js
+++ packages/email-all-chars-within-ascii/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { within } from "../dist/email-all-chars-within-ascii.esm.js";
 
