68a932af68c528488633327121363e22e6e4a1b5
Roy Revelt <roy@codsen.com>
2026-03-22 21:32:18 +0000

    docs: modernise the quick-take example

--- packages/email-comb/examples/_quickTake.js
+++ packages/email-comb/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 import { comb, defaults, version } from "../dist/email-comb.esm.js";
 
 const source = `<head>
