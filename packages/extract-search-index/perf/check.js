122434b73143193fea275c66e9a0c69bf43a21d4
Roy Revelt <roy@codsen.com>
2026-03-23 05:59:43 +0000

    chore: cleanup

--- packages/extract-search-index/perf/check.js
+++ packages/extract-search-index/perf/check.js
@@ -1,5 +1,5 @@
 // deps
-import path from "path";
+import path from "node:path";
 
 import { runPerf } from "../../../ops/scripts/perf.js";
 import { extract } from "../dist/extract-search-index.esm.js";
