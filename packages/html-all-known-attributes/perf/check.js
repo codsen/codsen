6214f8176d1cedb6aa133724dbbfa057605c2092
Roy Revelt <roy@codsen.com>
2026-03-26 05:34:32 +0000

    chore: cleanup

--- packages/html-all-known-attributes/perf/check.js
+++ packages/html-all-known-attributes/perf/check.js
@@ -1,5 +1,5 @@
 // deps
-import path from "path";
+import path from "node:path";
 
 import { runPerf } from "../../../ops/scripts/perf.js";
 import { allHtmlAttribs } from "../dist/html-all-known-attributes.esm.js";
