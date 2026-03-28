d354d7516c03ca36c3be2acbd1b1d0944f7ec7a3
Roy Revelt <roy@codsen.com>
2026-03-28 05:27:59 +0000

    tests: update tests

--- packages/html-crush/test/removeHTMLComments-nothing.js
+++ packages/html-crush/test/removeHTMLComments-nothing.js
@@ -1,12 +1,12 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { compare } from "../../../ops/helpers/shallow-compare.js";
 import { m } from "./util/util.js";
 
 // grouped tests
-test(`01 - ${`\u001b[${33}m${"html comments"}\u001b[${39}m`} - does nothing`, () => {
+test(`01 - html comments - does nothing`, () => {
   [
     "abc def",
     "!--",
