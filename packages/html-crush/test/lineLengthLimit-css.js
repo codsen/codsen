79ea22f3b6111dc14375e4a9353b1021a76072a4
Roy Revelt <roy@codsen.com>
2026-03-28 05:18:47 +0000

    tests: update tests

--- packages/html-crush/test/lineLengthLimit-css.js
+++ packages/html-crush/test/lineLengthLimit-css.js
@@ -1,11 +1,11 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { compare } from "../../../ops/helpers/shallow-compare.js";
 import { m } from "./util/util.js";
 
-test(`01 - ${`\u001b[${33}m${"css line length limit"}\u001b[${39}m`} - basic`, () => {
+test(`01 - css line length limit - basic`, () => {
   compare(
     ok,
     m(
