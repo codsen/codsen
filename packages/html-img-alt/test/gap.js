e1947f26445df2931a4737fe93bc9a395a62964b
Roy Revelt <roy@codsen.com>
2026-03-30 05:18:44 +0100

    tests: update gap

--- packages/html-img-alt/test/gap.js
+++ packages/html-img-alt/test/gap.js
@@ -1,6 +1,6 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { alts } from "../dist/html-img-alt.esm.js";
 
