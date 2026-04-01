5513b8876c49325a0d005459c8721043641768b9
Roy Revelt <roy@codsen.com>
2026-04-01 05:21:11 +0100

    tests: update tests

--- packages/html-table-patcher/test/between-tr-td.js
+++ packages/html-table-patcher/test/between-tr-td.js
@@ -1,13 +1,13 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { processThis, tiny } from "./util.js";
 
 // 02. type #2 - code between TR and TD
 // -----------------------------------------------------------------------------
 
-test(`01 - ${`\u001b[${36}m${"type 2"}\u001b[${39}m`}${`\u001b[${33}m${` - code between TR and ${`\u001b[${34}m${"TD"}\u001b[${39}m`}`}\u001b[${39}m`} - first TD after TR`, () => {
+test(`01 - type 2${`\u001b[${33}m${` - code between TR and ${`\u001b[${34}m${"TD"}\u001b[${39}m`}`}\u001b[${39}m`} - first TD after TR`, () => {
   equal(
     processThis(`<table>
   <tr>
@@ -33,7 +33,7 @@ test(`01 - ${`\u001b[${36}m${"type 2"}\u001b[${39}m`}${`\u001b[${33}m${` - code 
   );
 });
 
-test(`02 - ${`\u001b[${36}m${"type 2"}\u001b[${39}m`}${`\u001b[${33}m${` - code between TR and ${`\u001b[${34}m${"TD"}\u001b[${39}m`}`}\u001b[${39}m`} - colspan=2`, () => {
+test(`02 - type 2${`\u001b[${33}m${` - code between TR and ${`\u001b[${34}m${"TD"}\u001b[${39}m`}`}\u001b[${39}m`} - colspan=2`, () => {
   equal(
     processThis(`<table>
   <tr>
@@ -65,7 +65,7 @@ test(`02 - ${`\u001b[${36}m${"type 2"}\u001b[${39}m`}${`\u001b[${33}m${` - code 
   );
 });
 
-test(`03 - ${`\u001b[${36}m${"type 2"}\u001b[${39}m`}${`\u001b[${33}m${` - code between TR and ${`\u001b[${34}m${"TD"}\u001b[${39}m`}`}\u001b[${39}m`} - align="center", one TD`, () => {
+test(`03 - type 2${`\u001b[${33}m${` - code between TR and ${`\u001b[${34}m${"TD"}\u001b[${39}m`}`}\u001b[${39}m`} - align="center", one TD`, () => {
   equal(
     processThis(`<table>
   <tr>
@@ -91,7 +91,7 @@ test(`03 - ${`\u001b[${36}m${"type 2"}\u001b[${39}m`}${`\u001b[${33}m${` - code 
   );
 });
 
-test(`04 - ${`\u001b[${36}m${"type 2"}\u001b[${39}m`}${`\u001b[${33}m${` - code between TR and ${`\u001b[${34}m${"TD"}\u001b[${39}m`}`}\u001b[${39}m`} - align="center" on one of two TD's`, () => {
+test(`04 - type 2${`\u001b[${33}m${` - code between TR and ${`\u001b[${34}m${"TD"}\u001b[${39}m`}`}\u001b[${39}m`} - align="center" on one of two TD's`, () => {
   equal(
     processThis(`<table>
   <tr>
