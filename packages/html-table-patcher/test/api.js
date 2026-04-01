211e6d6ee53626027ec9e16ebe96fc61dfff93c0
Roy Revelt <roy@codsen.com>
2026-04-01 05:19:36 +0100

    test: update API test numbering

--- packages/html-table-patcher/test/api.js
+++ packages/html-table-patcher/test/api.js
@@ -1,28 +1,28 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
-import { patcher, defaults, version } from "../dist/html-table-patcher.esm.js";
+import { defaults, patcher, version } from "../dist/html-table-patcher.esm.js";
 
 // checking the API bits
 // -----------------------------------------------------------------------------
 
-test(`01 - ${`\u001b[${34}m${"API bits"}\u001b[${39}m`} - defaults`, () => {
+test(`01 - API bits - defaults`, () => {
   ok(typeof defaults === "object", "01.01");
   ok(Object.keys(defaults).length > 0, "01.02");
 });
 
-test(`02 - ${`\u001b[${34}m${"API bits"}\u001b[${39}m`} - version`, () => {
+test(`02 - API bits - version`, () => {
   match(version, /\d*\.\d*\.\d*/, "02.01");
 });
 
-test(`03 - ${`\u001b[${34}m${"API bits"}\u001b[${39}m`} - patcher()`, () => {
+test(`03 - API bits - patcher()`, () => {
   is(patcher(1).result, 1, "03.01");
   is(patcher(false).result, false, "03.02");
   is(patcher(null).result, null, "03.03");
 });
 
-test(`04 - ${`\u001b[${34}m${"API bits"}\u001b[${39}m`} - opts.opts.cssStylesContent`, () => {
+test(`04 - API bits - opts.opts.cssStylesContent`, () => {
   is(
     patcher("<a>", {
       cssStylesContent: 9,
