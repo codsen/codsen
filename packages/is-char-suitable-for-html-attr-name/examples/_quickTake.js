836787dcaf7ded31e6261cf7a6544b4fc77ba33a
Roy Revelt <roy@codsen.com>
2026-04-03 05:19:04 +0100

    docs: modernise the quick-take example

--- packages/is-char-suitable-for-html-attr-name/examples/_quickTake.js
+++ packages/is-char-suitable-for-html-attr-name/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { isAttrNameChar } from "../dist/is-char-suitable-for-html-attr-name.esm.js";
 
