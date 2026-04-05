82ba5b99c569977883b05f6a1a15b5f599fa6134
Roy Revelt <roy@codsen.com>
2026-04-05 21:06:20 +0100

    test: update the weird test numbering

--- packages/is-html-attribute-closing/test/weird.js
+++ packages/is-html-attribute-closing/test/weird.js
@@ -1,54 +1,55 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { isAttrClosing as isCl } from "../dist/is-html-attribute-closing.esm.js";
+
 // const BACKSLASH = "\u005C";
 
 // Weird cases
 // -----------------------------------------------------------------------------
 
-test(`01 - ${`\u001b[${34}m${"weird cases"}\u001b[${39}m`} - no input`, () => {
+test(`01 - weird cases - no input`, () => {
   not.ok(isCl(), "01.01");
 });
 
-test(`02 - ${`\u001b[${34}m${"weird cases"}\u001b[${39}m`} - input is not a string`, () => {
+test(`02 - weird cases - input is not a string`, () => {
   not.ok(isCl(2), "02.01");
 });
 
-test(`03 - ${`\u001b[${34}m${"weird cases"}\u001b[${39}m`} - input is empty string`, () => {
+test(`03 - weird cases - input is empty string`, () => {
   not.ok(isCl(""), "03.01");
 });
 
-test(`04 - ${`\u001b[${34}m${"weird cases"}\u001b[${39}m`} - 2nd arg is missing`, () => {
+test(`04 - weird cases - 2nd arg is missing`, () => {
   not.ok(isCl("a"), "04.01");
 });
 
-test(`05 - ${`\u001b[${34}m${"weird cases"}\u001b[${39}m`} - 2nd arg is not integer`, () => {
+test(`05 - weird cases - 2nd arg is not integer`, () => {
   not.ok(isCl("a", "a"), "05.01");
 });
 
-test(`06 - ${`\u001b[${34}m${"weird cases"}\u001b[${39}m`} - 3rd arg is missing`, () => {
+test(`06 - weird cases - 3rd arg is missing`, () => {
   not.ok(isCl("a", 0), "06.01");
 });
 
-test(`07 - ${`\u001b[${34}m${"weird cases"}\u001b[${39}m`} - 3rd arg is not integer`, () => {
+test(`07 - weird cases - 3rd arg is not integer`, () => {
   not.ok(isCl("a", 0, "a"), "07.01");
 });
 
-test(`08 - ${`\u001b[${34}m${"weird cases"}\u001b[${39}m`} - no character in string at what's given by 2nd arg`, () => {
+test(`08 - weird cases - no character in string at what's given by 2nd arg`, () => {
   not.ok(isCl("a", 99, 100), "08.01");
 });
 
-test(`09 - ${`\u001b[${34}m${"weird cases"}\u001b[${39}m`} - no character in string at what's given by 3rd arg`, () => {
+test(`09 - weird cases - no character in string at what's given by 3rd arg`, () => {
   not.ok(isCl("a", 0, 100), "09.01");
 });
 
-test(`10 - ${`\u001b[${34}m${"weird cases"}\u001b[${39}m`} - indexes equal`, () => {
+test(`10 - weird cases - indexes equal`, () => {
   not.ok(isCl("abcde", 2, 2), "10.01");
 });
 
-test(`11 - ${`\u001b[${34}m${"weird cases"}\u001b[${39}m`} - 3rd > 2nd`, () => {
+test(`11 - weird cases - 3rd > 2nd`, () => {
   not.ok(isCl("abcde", 2, 1), "11.01");
 });
 
