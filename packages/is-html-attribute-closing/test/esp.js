d34ed0883439af955711979981f8d4fecfcdba7e
Roy Revelt <roy@codsen.com>
2026-04-05 20:50:52 +0100

    tests: update esp

--- packages/is-html-attribute-closing/test/esp.js
+++ packages/is-html-attribute-closing/test/esp.js
@@ -1,19 +1,20 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { isAttrClosing as isCl } from "../dist/is-html-attribute-closing.esm.js";
+
 // const BACKSLASH = "\u005C";
 
 // ESP code cases
 // -----------------------------------------------------------------------------
 
-test(`01 - ${`\u001b[${32}m${"ESP"}\u001b[${39}m`} - the Killer Triplet`, () => {
+test(`01 - ESP - the Killer Triplet`, () => {
   let str = '<a b="c{{ z("\'") }}"><b>';
   ok(isCl(str, 5, 19), "01.01");
 });
 
-test(`02 - ${`\u001b[${32}m${"ESP"}\u001b[${39}m`} - Ruby ERB`, () => {
+test(`02 - ESP - Ruby ERB`, () => {
   let str = '<a href="https://abc?p1=<%= @p1 %>&p2=<%= @p2 %>">';
   ok(isCl(str, 8, 48), "02.01");
 });
