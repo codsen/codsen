1dc65b3d2357c69b340349beb853e34674c92b37
Roy Revelt <roy@codsen.com>
2026-04-05 20:50:37 +0100

    test: update double wrapped test numbering

--- packages/is-html-attribute-closing/test/double-wrapped.js
+++ packages/is-html-attribute-closing/test/double-wrapped.js
@@ -1,8 +1,9 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { isAttrClosing as isCl } from "../dist/is-html-attribute-closing.esm.js";
+
 // const BACKSLASH = "\u005C";
 
 //
