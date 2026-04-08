0cbc8fd38ca902870fe622f6716c88bf000d26fb
Roy Revelt <roy@codsen.com>
2026-04-08 05:55:57 +0100

    chore: cleanup

--- packages/is-html-tag-opening/perf/check.js
+++ packages/is-html-tag-opening/perf/check.js
@@ -1,12 +1,12 @@
 // deps
-import path from "path";
+import path from "node:path";
 
 import { runPerf } from "../../../ops/scripts/perf.js";
 import { isOpening } from "../dist/is-html-tag-opening.esm.js";
 
 const callerDir = path.resolve(".");
 
-const testme = () => isOpening("zzz<img           /    >zzz");
+const testme = () => isOpening("zzz<img           /    >zzz", 3);
 
 // action
 runPerf(testme, callerDir);
