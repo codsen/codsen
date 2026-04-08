9fc4e76b1a48322f85ca99bb00c95bb9cb2f0eba
Roy Revelt <roy@codsen.com>
2026-04-08 05:47:25 +0100

    tests: update missing bits

--- packages/is-html-tag-opening/test/missing-bits.js
+++ packages/is-html-tag-opening/test/missing-bits.js
@@ -1,13 +1,13 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { isOpening } from "../dist/is-html-tag-opening.esm.js";
 
 // missing bits
 // -----------------------------------------------------------------------------
 
-test(`01 - ${`\u001b[${36}m${"broken code"}\u001b[${39}m`} - quotes missing`, () => {
+test(`01 - broken code - quotes missing`, () => {
   let s1 = '<abc de=fg hi="jkl">';
   not.ok(isOpening(s1, 0), "01.01");
   ok(
