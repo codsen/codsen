c55e8407f83e40f6dca7d154394ca94a20947d0b
Roy Revelt <roy@codsen.com>
2026-04-17 05:47:24 +0100

    docs: update sort all objects sync example

--- packages/json-comb-core/examples/sortAllObjectsSync.js
+++ packages/json-comb-core/examples/sortAllObjectsSync.js
@@ -1,15 +1,15 @@
 // Synchronous `getKeysetSync()`
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import {
-  getKeysetSync,
-  getKeyset,
   enforceKeyset,
   enforceKeysetSync,
-  sortAllObjectsSync,
-  noNewKeysSync,
   findUnusedSync,
+  getKeyset,
+  getKeysetSync,
+  noNewKeysSync,
+  sortAllObjectsSync,
 } from "../dist/json-comb-core.esm.js";
 
 assert.deepEqual(
