001590539e57a13fc3d032bf91dcb613b60816ca
Roy Revelt <roy@codsen.com>
2026-04-29 05:26:56 +0100

    docs: modernise the quick-take example

--- packages/lerna-clean-changelogs/examples/_quickTake.js
+++ packages/lerna-clean-changelogs/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { cleanChangelogs } from "../dist/lerna-clean-changelogs.esm.js";
 
