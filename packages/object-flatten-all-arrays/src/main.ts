e1cbcd81e72719374b2357a4847ed8edd2274ea0
Roy Revelt <roy@codsen.com>
2026-05-09 20:51:56 +0100

    perf: flatten arrays from a single cloned input

--- packages/object-flatten-all-arrays/src/main.ts
+++ packages/object-flatten-all-arrays/src/main.ts
@@ -1,8 +1,5 @@
+import { deepClone as clone, isPlainObject as isObj } from "codsen-utils";
 import { merge } from "lodash-es";
-import rfdc from "rfdc";
-
-const clone = rfdc();
-import { isPlainObject as isObj } from "codsen-utils";
 
 import { version as v } from "../package.json";
 
@@ -19,72 +16,50 @@ const defaults: Opts = {
 };
 
 function flattenAllArrays(input: Obj, opts?: Partial<Opts>): Obj {
-  //
-  // internal functions
-  // ==================
+  const resolvedOpts: Opts = { ...defaults, ...opts };
 
-  function arrayContainsStr(arr: any[]): boolean {
-    return arr.some((val) => typeof val === "string");
-  }
+  function flattenValue(incoming: any): any {
+    if (Array.isArray(incoming)) {
+      if (
+        resolvedOpts.flattenArraysContainingStringsToBeEmpty &&
+        incoming.some((value) => typeof value === "string")
+      ) {
+        return [];
+      }
 
-  // setup
-  // =====
+      let firstObjectIndex = -1;
+      let combinedObject = {};
+      for (let i = 0; i < incoming.length; i++) {
+        if (isObj(incoming[i])) {
+          combinedObject = merge(combinedObject, incoming[i]);
+          if (firstObjectIndex === -1) {
+            firstObjectIndex = i;
+          } else {
+            incoming.splice(i, 1);
+            i -= 1;
+          }
+        }
+      }
+      if (firstObjectIndex !== -1) {
+        incoming[firstObjectIndex] = combinedObject;
+      }
 
-  let resolvedOpts: Opts = { ...defaults, ...opts };
-
-  let incomingObj = clone(input);
-  let isFirstObj;
-  let combinedObj;
-  let firstObjIndex;
-
-  // action
-  // ======
-
-  // 1. check current
-  if (Array.isArray(incomingObj)) {
-    if (
-      resolvedOpts.flattenArraysContainingStringsToBeEmpty &&
-      arrayContainsStr(incomingObj)
-    ) {
-      return [];
-    }
-    isFirstObj = null;
-    combinedObj = {};
-    firstObjIndex = 0;
-    for (let i = 0, len = incomingObj.length; i < len; i++) {
-      if (isObj(incomingObj[i])) {
-        combinedObj = merge(combinedObj, incomingObj[i]);
-        if (isFirstObj === null) {
-          isFirstObj = true;
-          firstObjIndex = i;
-        } else {
-          incomingObj.splice(i, 1);
-          i -= 1;
+      for (let i = 0; i < incoming.length; i++) {
+        if (isObj(incoming[i]) || Array.isArray(incoming[i])) {
+          incoming[i] = flattenValue(incoming[i]);
+        }
+      }
+    } else if (isObj(incoming)) {
+      for (const key of Object.keys(incoming)) {
+        if (isObj(incoming[key]) || Array.isArray(incoming[key])) {
+          incoming[key] = flattenValue(incoming[key]);
         }
       }
     }
-    if (isFirstObj !== null) {
-      incomingObj[firstObjIndex] = clone(combinedObj);
-    }
+    return incoming;
   }
-  // 2. traverse deeper
-  if (isObj(incomingObj)) {
-    Object.keys(incomingObj).forEach((key) => {
-      if (isObj(incomingObj[key]) || Array.isArray(incomingObj[key])) {
-        incomingObj[key] = flattenAllArrays(
-          incomingObj[key] as Obj,
-          resolvedOpts,
-        );
-      }
-    });
-  } else if (Array.isArray(incomingObj)) {
-    incomingObj.forEach((_el, i) => {
-      if (isObj(incomingObj[i]) || Array.isArray(incomingObj[i])) {
-        incomingObj[i] = flattenAllArrays(incomingObj[i], resolvedOpts);
-      }
-    });
-  }
-  return incomingObj;
+
+  return flattenValue(clone(input));
 }
 
-export { flattenAllArrays, defaults, version };
+export { defaults, flattenAllArrays, version };
