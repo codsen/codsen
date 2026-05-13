796c89f9a96d3c9b59fc66d1de295ac202f48d92
Roy Revelt <roy@codsen.com>
2026-05-13 05:18:14 +0100

    chore: cleanup

--- packages/object-no-new-keys/perf/check.js
+++ packages/object-no-new-keys/perf/check.js
@@ -1,5 +1,5 @@
 // deps
-import path from "path";
+import path from "node:path";
 
 import { runPerf } from "../../../ops/scripts/perf.js";
 import { noNewKeys } from "../dist/object-no-new-keys.esm.js";
