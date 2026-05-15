4a5229807d939c27df021ed22d252d3ae9188c23
Roy Revelt <roy@codsen.com>
2026-05-15 05:53:40 +0100

    docs: modernise the quick-take example

--- packages/object-set-all-values-to/examples/_quickTake.js
+++ packages/object-set-all-values-to/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { setAllValuesTo } from "../dist/object-set-all-values-to.esm.js";
 
