80acd0a7f7e2eb003f1b7be258ef86fab3bb0602
Roy Revelt <roy@codsen.com>
2026-05-15 05:54:07 +0100

    chore: cleanup

--- packages/object-set-all-values-to/perf/check.js
+++ packages/object-set-all-values-to/perf/check.js
@@ -1,5 +1,5 @@
 // deps
-import path from "path";
+import path from "node:path";
 
 import { runPerf } from "../../../ops/scripts/perf.js";
 import { setAllValuesTo } from "../dist/object-set-all-values-to.esm.js";
