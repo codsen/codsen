615eeb63814554283e5eb0a7f01249247cce0eae
Roy Revelt <roy@codsen.com>
2026-05-24 05:15:25 +0100

    docs: modernise the quick-take example

--- packages/ranges-is-index-within/examples/_quickTake.js
+++ packages/ranges-is-index-within/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { isIndexWithin } from "../dist/ranges-is-index-within.esm.js";
 
