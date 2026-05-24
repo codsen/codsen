bf5fb1f371ff351caf0900e93b5c75b45c96e461
Roy Revelt <roy@codsen.com>
2026-05-24 05:15:45 +0100

    chore: cleanup

--- packages/ranges-is-index-within/perf/check.js
+++ packages/ranges-is-index-within/perf/check.js
@@ -1,5 +1,5 @@
 // deps
-import path from "path";
+import path from "node:path";
 
 import { runPerf } from "../../../ops/scripts/perf.js";
 import { isIndexWithin } from "../dist/ranges-is-index-within.esm.js";
@@ -8,7 +8,7 @@ const callerDir = path.resolve(".");
 
 const testme = () =>
   isIndexWithin(
-    5,
+    57,
     [
       [2, 4],
       [6, 8],
