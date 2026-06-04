8f77437005d6e8a65610509336c0b83682ef40f1
Roy Revelt <roy@codsen.com>
2026-06-04 06:00:32 +0100

    docs: modernise the quick-take example

--- packages/regex-is-jinja-nunjucks/examples/_quickTake.js
+++ packages/regex-is-jinja-nunjucks/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { isJinjaNunjucksRegex } from "../dist/regex-is-jinja-nunjucks.esm.js";
 
