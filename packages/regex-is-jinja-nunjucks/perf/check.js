294a6b368aadb1b87ccefd58c26baf84beaa8e58
Roy Revelt <roy@codsen.com>
2026-06-04 06:01:06 +0100

    chore: cleanup

--- packages/regex-is-jinja-nunjucks/perf/check.js
+++ packages/regex-is-jinja-nunjucks/perf/check.js
@@ -1,5 +1,5 @@
 // deps
-import path from "path";
+import path from "node:path";
 
 import { runPerf } from "../../../ops/scripts/perf.js";
 import { isJinjaNunjucksRegex } from "../dist/regex-is-jinja-nunjucks.esm.js";
