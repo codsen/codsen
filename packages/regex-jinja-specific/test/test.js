149708aa217504ee0d02cd16189348e9bd402c56
Roy Revelt <roy@codsen.com>
2026-06-05 05:49:51 +0100

    test: update the test numbering

--- packages/regex-jinja-specific/test/test.js
+++ packages/regex-jinja-specific/test/test.js
@@ -1,26 +1,26 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { isJinjaSpecific } from "../dist/regex-jinja-specific.esm.js";
 
-test("01 is not Jinja", () => {
+test("01 - is not Jinja", () => {
   not.match("", isJinjaSpecific(), "01.01");
   not.match("\n", isJinjaSpecific(), "01.02");
   not.match("abc", isJinjaSpecific(), "01.03");
   not.match("<html><div></html>", isJinjaSpecific(), "01.04");
 });
 
-test("02 maybe Jinja", () => {
+test("02 - maybe Jinja", () => {
   not.match("{{ var1 }}", isJinjaSpecific(), "02.01");
   not.match("{% if something %}", isJinjaSpecific(), "02.02");
 });
 
-test("03 namespaces", () => {
+test("03 - namespaces", () => {
   match("{% set x = namespace(blablabla) %}", isJinjaSpecific(), "03.01");
 });
 
-test("04 backwards order", () => {
+test("04 - backwards order", () => {
   match("{{'oodles' if crambles else 'brambles'}}", isJinjaSpecific(), "04.01");
 });
 
