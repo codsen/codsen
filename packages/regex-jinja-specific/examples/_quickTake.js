a887a3377ca7430143fcdcf190a7b5967a38777a
Roy Revelt <roy@codsen.com>
2026-06-05 05:53:59 +0100

    docs: modernise the quick-take example

--- packages/regex-jinja-specific/examples/_quickTake.js
+++ packages/regex-jinja-specific/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { isJinjaSpecific } from "../dist/regex-jinja-specific.esm.js";
 
