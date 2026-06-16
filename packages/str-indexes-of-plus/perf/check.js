ee599a79a697d2124fb91292aacba4bde88a8cbc
Roy Revelt <roy@codsen.com>
2026-06-16 06:24:34 +0100

    chore: cleanup

--- packages/str-indexes-of-plus/perf/check.js
+++ packages/str-indexes-of-plus/perf/check.js
@@ -1,5 +1,5 @@
 // deps
-import path from "path";
+import path from "node:path";
 
 import { runPerf } from "../../../ops/scripts/perf.js";
 import { strIndexesOfPlus } from "../dist/str-indexes-of-plus.esm.js";
