cb7b0ae84ae61afa6a4a8a427cb09a35173f9d3e
Roy Revelt <roy@codsen.com>
2026-06-19 05:30:10 +0100

    chore: cleanup

--- packages/string-apostrophes/perf/check.js
+++ packages/string-apostrophes/perf/check.js
@@ -1,5 +1,5 @@
 // deps
-import path from "path";
+import path from "node:path";
 
 import { runPerf } from "../../../ops/scripts/perf.js";
 import { convertAll } from "../dist/string-apostrophes.esm.js";
@@ -10,8 +10,8 @@ const testme = () =>
   convertAll(
     "Welcome to Website Name! Company Name, Inc. ('Company Name' or 'Company') recommends that you read the following terms and conditions carefully.",
     {
-      convertApostrophes: 1,
-      convertEntities: 0,
+      convertApostrophes: true,
+      convertEntities: false,
     },
   );
 
