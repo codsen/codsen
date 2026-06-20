f1869ccb66b97759d548f7e3368f252c022bd5c6
Roy Revelt <roy@codsen.com>
2026-06-20 20:54:35 +0100

    chore: cleanup

--- packages/string-character-is-astral-surrogate/perf/check.js
+++ packages/string-character-is-astral-surrogate/perf/check.js
@@ -1,5 +1,5 @@
 // deps
-import path from "path";
+import path from "node:path";
 
 import { runPerf } from "../../../ops/scripts/perf.js";
 import { isHighSurrogate } from "../dist/string-character-is-astral-surrogate.esm.js";
