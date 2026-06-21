7b781bde8165adee4c951f76081079459fa1a180
Roy Revelt <roy@codsen.com>
2026-06-21 05:41:30 +0100

    tests: update tests

--- packages/string-collapse-leading-whitespace/test/quick-ends.js
+++ packages/string-collapse-leading-whitespace/test/quick-ends.js
@@ -1,75 +1,75 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { collWhitespace as c } from "../dist/string-collapse-leading-whitespace.esm.js";
 
-test("01 - empty input", () => {
-  equal(c(""), "", "01.01");
+test("001 - empty input", () => {
+  equal(c(""), "", "001.01");
 });
 
-test("02 - all whitespace", () => {
-  equal(c("    "), " ", "02.01");
+test("002 - all whitespace", () => {
+  equal(c("    "), " ", "002.01");
 });
 
-test("03 - all whitespace", () => {
-  equal(c("\t"), " ", "03.01");
+test("003 - all whitespace", () => {
+  equal(c("\t"), " ", "003.01");
 });
 
-test("04 - all whitespace", () => {
-  equal(c("    ", 1), " ", "04.01");
+test("004 - all whitespace", () => {
+  equal(c("    ", 1), " ", "004.01");
 });
 
-test("05 - all whitespace", () => {
-  equal(c("\t", 1), " ", "05.01");
+test("005 - all whitespace", () => {
+  equal(c("\t", 1), " ", "005.01");
 });
 
-test("06 - all whitespace", () => {
-  equal(c("    ", 2), " ", "06.01");
+test("006 - all whitespace", () => {
+  equal(c("    ", 2), " ", "006.01");
 });
 
-test("07 - all whitespace", () => {
-  equal(c("\t", 2), " ", "07.01");
+test("007 - all whitespace", () => {
+  equal(c("\t", 2), " ", "007.01");
 });
 
-test("08 - all whitespace", () => {
-  equal(c("  \n\n  "), "\n", "08.01");
+test("008 - all whitespace", () => {
+  equal(c("  \n\n  "), "\n", "008.01");
 });
 
-test("09 - all whitespace", () => {
-  equal(c("  \n\n  ", 1), "\n", "09.01");
+test("009 - all whitespace", () => {
+  equal(c("  \n\n  ", 1), "\n", "009.01");
 });
 
-test("10 - all whitespace", () => {
-  equal(c("  \n\n  ", 2), "\n\n", "10.01");
+test("010 - all whitespace", () => {
+  equal(c("  \n\n  ", 2), "\n\n", "010.01");
 });
 
-test("11 - all whitespace", () => {
-  equal(c("  \n\n  ", 9), "\n\n", "11.01");
+test("011 - all whitespace", () => {
+  equal(c("  \n\n  ", 9), "\n\n", "011.01");
 });
 
-test("12 - all whitespace", () => {
-  equal(c("\n"), "\n", "12.01");
+test("012 - all whitespace", () => {
+  equal(c("\n"), "\n", "012.01");
 });
 
-test("13 - all whitespace", () => {
-  equal(c("\n", 1), "\n", "13.01");
+test("013 - all whitespace", () => {
+  equal(c("\n", 1), "\n", "013.01");
 });
 
-test("14 - all whitespace", () => {
-  equal(c("\n", 2), "\n", "14.01");
+test("014 - all whitespace", () => {
+  equal(c("\n", 2), "\n", "014.01");
 });
 
-test("15 - all whitespace", () => {
-  equal(c("\n\n", 2), "\n\n", "15.01");
+test("015 - all whitespace", () => {
+  equal(c("\n\n", 2), "\n\n", "015.01");
 });
 
-test("16 - all whitespace", () => {
-  equal(c("\n\n", 3), "\n\n", "16.01");
+test("016 - all whitespace", () => {
+  equal(c("\n\n", 3), "\n\n", "016.01");
 });
 
-test("17 - all whitespace", () => {
-  equal(c("\n \n\n\n", 5), "\n\n\n\n", "17.01");
+test("017 - all whitespace", () => {
+  equal(c("\n \n\n\n", 5), "\n\n\n\n", "017.01");
 });
 
 test.run();
