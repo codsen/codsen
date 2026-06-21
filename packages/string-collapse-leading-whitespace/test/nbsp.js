8f8fc0a6b77a7d14b2355b5b3924a88cafb93181
Roy Revelt <roy@codsen.com>
2026-06-21 05:41:18 +0100

    tests: update nbsp

--- packages/string-collapse-leading-whitespace/test/nbsp.js
+++ packages/string-collapse-leading-whitespace/test/nbsp.js
@@ -1,81 +1,81 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { collWhitespace as c } from "../dist/string-collapse-leading-whitespace.esm.js";
 
 const rawNbsp = "\u00A0";
 
-test("01 - nbsp - left side - blank", () => {
-  equal(c(`${rawNbsp}zzz`), `${rawNbsp}zzz`, "01.01");
+test("001 - nbsp - left side - blank", () => {
+  equal(c(`${rawNbsp}zzz`), `${rawNbsp}zzz`, "001.01");
 });
 
-test("02 - nbsp - left side - space + nbsp", () => {
-  equal(c(` ${rawNbsp}zzz`), `${rawNbsp}zzz`, "02.01");
+test("002 - nbsp - left side - space + nbsp", () => {
+  equal(c(` ${rawNbsp}zzz`), `${rawNbsp}zzz`, "002.01");
 });
 
-test("03 - nbsp - left side - two spaces", () => {
-  equal(c(`  ${rawNbsp}zzz`), `${rawNbsp}zzz`, "03.01");
+test("003 - nbsp - left side - two spaces", () => {
+  equal(c(`  ${rawNbsp}zzz`), `${rawNbsp}zzz`, "003.01");
 });
 
-test("04 - nbsp - left side - nbsp + space", () => {
-  equal(c(`${rawNbsp} zzz`), `${rawNbsp} zzz`, "04.01");
+test("004 - nbsp - left side - nbsp + space", () => {
+  equal(c(`${rawNbsp} zzz`), `${rawNbsp} zzz`, "004.01");
 });
 
-test("05 - nbsp - left side - nbsp + two spaces", () => {
-  equal(c(`${rawNbsp}  zzz`), `${rawNbsp} zzz`, "05.01");
+test("005 - nbsp - left side - nbsp + two spaces", () => {
+  equal(c(`${rawNbsp}  zzz`), `${rawNbsp} zzz`, "005.01");
 });
 
-test("06 - nbsp - left side - eol + nbsp", () => {
-  equal(c(`\n${rawNbsp}zzz`), `\n${rawNbsp}zzz`, "06.01");
+test("006 - nbsp - left side - eol + nbsp", () => {
+  equal(c(`\n${rawNbsp}zzz`), `\n${rawNbsp}zzz`, "006.01");
 });
 
-test("07 - nbsp - left side - nbsp + eol", () => {
-  equal(c(`${rawNbsp}\nzzz`), `${rawNbsp}\nzzz`, "07.01");
+test("007 - nbsp - left side - nbsp + eol", () => {
+  equal(c(`${rawNbsp}\nzzz`), `${rawNbsp}\nzzz`, "007.01");
 });
 
-test("08 - nbsp - left side - multiple eols", () => {
-  equal(c(`\n\n${rawNbsp}\nzzz`, 3), `\n\n${rawNbsp}\nzzz`, "08.01");
+test("008 - nbsp - left side - multiple eols", () => {
+  equal(c(`\n\n${rawNbsp}\nzzz`, 3), `\n\n${rawNbsp}\nzzz`, "008.01");
 });
 
-test("09 - nbsp - left side - multiple spaced eols", () => {
-  equal(c(`  \n \n   ${rawNbsp}\nzzz`, 3), `\n\n${rawNbsp}\nzzz`, "09.01");
+test("009 - nbsp - left side - multiple spaced eols", () => {
+  equal(c(`  \n \n   ${rawNbsp}\nzzz`, 3), `\n\n${rawNbsp}\nzzz`, "009.01");
 });
 
-test("10 - nbsp - right side - blank", () => {
-  equal(c(`zzz${rawNbsp}`), `zzz${rawNbsp}`, "10.01");
+test("010 - nbsp - right side - blank", () => {
+  equal(c(`zzz${rawNbsp}`), `zzz${rawNbsp}`, "010.01");
 });
 
-test("11 - nbsp - right side - nbsp + space", () => {
-  equal(c(`zzz${rawNbsp} `), `zzz${rawNbsp}`, "11.01");
+test("011 - nbsp - right side - nbsp + space", () => {
+  equal(c(`zzz${rawNbsp} `), `zzz${rawNbsp}`, "011.01");
 });
 
-test("12 - nbsp - right side - nbsp + two spaces", () => {
-  equal(c(`zzz${rawNbsp}  `), `zzz${rawNbsp}`, "12.01");
+test("012 - nbsp - right side - nbsp + two spaces", () => {
+  equal(c(`zzz${rawNbsp}  `), `zzz${rawNbsp}`, "012.01");
 });
 
-test("13 - nbsp - right side - space + nbsp", () => {
-  equal(c(`zzz ${rawNbsp}`), `zzz ${rawNbsp}`, "13.01");
+test("013 - nbsp - right side - space + nbsp", () => {
+  equal(c(`zzz ${rawNbsp}`), `zzz ${rawNbsp}`, "013.01");
 });
 
-test("14 - nbsp - right side - two spaces + nbsp", () => {
-  equal(c(`zzz  ${rawNbsp}`), `zzz ${rawNbsp}`, "14.01");
+test("014 - nbsp - right side - two spaces + nbsp", () => {
+  equal(c(`zzz  ${rawNbsp}`), `zzz ${rawNbsp}`, "014.01");
 });
 
-test("15 - nbsp - right side - nbsp + eol", () => {
-  equal(c(`zzz${rawNbsp}\n`), `zzz${rawNbsp}\n`, "15.01");
+test("015 - nbsp - right side - nbsp + eol", () => {
+  equal(c(`zzz${rawNbsp}\n`), `zzz${rawNbsp}\n`, "015.01");
 });
 
-test("16 - nbsp - right side - eol + nbsp", () => {
-  equal(c(`zzz\n${rawNbsp}`), `zzz\n${rawNbsp}`, "16.01");
+test("016 - nbsp - right side - eol + nbsp", () => {
+  equal(c(`zzz\n${rawNbsp}`), `zzz\n${rawNbsp}`, "016.01");
 });
 
-test("17 - nbsp - right side - mulitple eols", () => {
-  equal(c(`zzz\n${rawNbsp}\n\n`, 3), `zzz\n${rawNbsp}\n\n`, "17.01");
+test("017 - nbsp - right side - mulitple eols", () => {
+  equal(c(`zzz\n${rawNbsp}\n\n`, 3), `zzz\n${rawNbsp}\n\n`, "017.01");
 });
 
-test("18 - nbsp - right side - multiple spaced eols", () => {
-  equal(c(`zzz\n${rawNbsp}  \n \n   `, 3), `zzz\n${rawNbsp}\n\n`, "18.01");
+test("018 - nbsp - right side - multiple spaced eols", () => {
+  equal(c(`zzz\n${rawNbsp}  \n \n   `, 3), `zzz\n${rawNbsp}\n\n`, "018.01");
 });
 
 test.run();
