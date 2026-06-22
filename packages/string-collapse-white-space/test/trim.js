200ec9cb3c0df0ecc3e8e273350fe6cbaea47a99
Roy Revelt <roy@codsen.com>
2026-06-22 05:31:10 +0100

    tests: update

--- packages/string-collapse-white-space/test/trim.js
+++ packages/string-collapse-white-space/test/trim.js
@@ -1,60 +1,80 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import { collapse } from "../dist/string-collapse-white-space.esm.js";
 import { mixer } from "./util/util.js";
 
-test("01  - one line, trimLines=false", () => {
+test("01 - one line, trimLines=false", () => {
   let input = "  a  ";
   mixer({
     trimStart: false,
     trimEnd: false,
     trimLines: false,
   }).forEach((opt) => {
-    equal(collapse(input, opt).result, " a ", JSON.stringify(opt, null, 0));
+    equal(
+      collapse(input, opt).result,
+      " a ",
+      `01.01 - ${JSON.stringify(opt, null, 0)}`,
+    );
   });
 });
 
-test("02  - one line, trimLines=false", () => {
+test("02 - one line, trimLines=false", () => {
   let input = "  a  ";
   mixer({
     trimStart: true,
     trimEnd: false,
     trimLines: false,
   }).forEach((opt) => {
-    equal(collapse(input, opt).result, "a ", JSON.stringify(opt, null, 0));
+    equal(
+      collapse(input, opt).result,
+      "a ",
+      `02.01 - ${JSON.stringify(opt, null, 0)}`,
+    );
   });
 });
 
-test("03  - one line, trimLines=false", () => {
+test("03 - one line, trimLines=false", () => {
   let input = "  a  ";
   mixer({
     trimStart: false,
     trimEnd: true,
     trimLines: false,
   }).forEach((opt) => {
-    equal(collapse(input, opt).result, " a", JSON.stringify(opt, null, 0));
+    equal(
+      collapse(input, opt).result,
+      " a",
+      `03.01 - ${JSON.stringify(opt, null, 0)}`,
+    );
   });
 });
 
-test("04  - one line, trimLines=false", () => {
+test("04 - one line, trimLines=false", () => {
   let input = "  a  ";
   mixer({
     trimStart: true,
     trimEnd: true,
     trimLines: false,
   }).forEach((opt) => {
-    equal(collapse(input, opt).result, "a", JSON.stringify(opt, null, 0));
+    equal(
+      collapse(input, opt).result,
+      "a",
+      `04.01 - ${JSON.stringify(opt, null, 0)}`,
+    );
   });
 });
 
-test("05  - one line, trimLines=true", () => {
+test("05 - one line, trimLines=true", () => {
   let input = "  a  ";
   mixer({
     trimLines: true,
   }).forEach((opt) => {
-    equal(collapse(input, opt).result, "a", JSON.stringify(opt, null, 0));
+    equal(
+      collapse(input, opt).result,
+      "a",
+      `05.01 - ${JSON.stringify(opt, null, 0)}`,
+    );
   });
 });
 
