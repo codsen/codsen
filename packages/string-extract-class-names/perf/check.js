8c32ecc7b74fd96aa9ccfe026121a1217f88751c
Roy Revelt <roy@codsen.com>
2026-07-05 05:36:32 +0100

    chore: cleanup

--- packages/string-extract-class-names/perf/check.js
+++ packages/string-extract-class-names/perf/check.js
@@ -1,13 +1,12 @@
 // deps
-import path from "path";
+import path from "node:path";
 
 import { runPerf } from "../../../ops/scripts/perf.js";
 import { extract } from "../dist/string-extract-class-names.esm.js";
 
 const callerDir = path.resolve(".");
 
-const testme = () =>
-  extract("p#id-name:lang(it) p#id-name-other:lang(en)", true);
+const testme = () => extract("p#id-name:lang(it) p#id-name-other:lang(en)");
 
 // action
 runPerf(testme, callerDir);
