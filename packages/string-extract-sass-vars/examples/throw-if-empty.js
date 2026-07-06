b7f09c400c9b77f339f0c40a6b6b4516c50955c2
Roy Revelt <roy@codsen.com>
2026-07-06 05:44:56 +0100

    docs: update example

--- packages/string-extract-sass-vars/examples/throw-if-empty.js
+++ packages/string-extract-sass-vars/examples/throw-if-empty.js
@@ -1,6 +1,6 @@
 // Raises alarm if variables file has been wiped
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { extractVars } from "../dist/string-extract-sass-vars.esm.js";
 
