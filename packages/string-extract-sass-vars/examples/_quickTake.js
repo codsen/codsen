cee4f610287e322a9bee68842cbcb0c55c060628
Roy Revelt <roy@codsen.com>
2026-07-06 05:43:53 +0100

    docs: modernise the quick-take example

--- packages/string-extract-sass-vars/examples/_quickTake.js
+++ packages/string-extract-sass-vars/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { extractVars } from "../dist/string-extract-sass-vars.esm.js";
 
