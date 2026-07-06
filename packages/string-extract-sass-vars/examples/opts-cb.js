e4c4efe64ce2eb4f1a9522cc5f692c6155041730
Roy Revelt <roy@codsen.com>
2026-07-06 05:44:25 +0100

    docs: update example

--- packages/string-extract-sass-vars/examples/opts-cb.js
+++ packages/string-extract-sass-vars/examples/opts-cb.js
@@ -1,9 +1,10 @@
 // Convert 3-digit color hex codes to 6-digit
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 import { conv } from "color-shorthand-hex-to-six-digit";
 
 import { extractVars } from "../dist/string-extract-sass-vars.esm.js";
+
 // import "color-shorthand-hex-to-six-digit" to convert three-digit colour hex
 // codes to six-digit:
 
