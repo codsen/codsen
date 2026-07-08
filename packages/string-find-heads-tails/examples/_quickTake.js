c266c17496cde8902e29ddbc0e687564a2ed80c2
Roy Revelt <roy@codsen.com>
2026-07-08 05:55:11 +0100

    docs: modernise the quick-take example

--- packages/string-find-heads-tails/examples/_quickTake.js
+++ packages/string-find-heads-tails/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { strFindHeadsTails } from "../dist/string-find-heads-tails.esm.js";
 
