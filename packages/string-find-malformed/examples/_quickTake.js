cc2e116f938fa7d9ca1572778ec5fcac8181ddd7
Roy Revelt <roy@codsen.com>
2026-07-11 05:19:29 +0100

    docs: modernise the quick-take example

--- packages/string-find-malformed/examples/_quickTake.js
+++ packages/string-find-malformed/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { findMalformed } from "../dist/string-find-malformed.esm.js";
 
