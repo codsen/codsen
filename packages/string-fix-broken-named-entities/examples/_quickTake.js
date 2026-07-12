79b5e1d57d9eaaec27bde2d3395ad72b79c945e8
Roy Revelt <roy@codsen.com>
2026-07-12 21:13:30 +0100

    docs: modernise quick-take example

--- packages/string-fix-broken-named-entities/examples/_quickTake.js
+++ packages/string-fix-broken-named-entities/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 import { rApply } from "ranges-apply";
 
 import { fixEnt } from "../dist/string-fix-broken-named-entities.esm.js";
