493fae7086a03d6640d46d06fd784329f48de847
Roy Revelt <roy@codsen.com>
2026-07-13 05:10:12 +0100

    test: update the edge test numbering

--- packages/string-left-right/test/edge.js
+++ packages/string-left-right/test/edge.js
@@ -1,13 +1,13 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
 import {
   left,
-  // leftStopAtNewLines,
-  right,
   // rightStopAtNewLines,
   leftSeq,
+  // leftStopAtNewLines,
+  right,
   rightSeq,
   // chompLeft,
   // chompRight,
