5bae483c9b7a5a39a49a03a697f0c1c93df5cf60
Roy Revelt <roy@codsen.com>
2026-07-18 20:56:00 +0100

    docs: modernise the quick-take example

--- packages/string-remove-duplicate-heads-tails/examples/_quickTake.js
+++ packages/string-remove-duplicate-heads-tails/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { remDup } from "../dist/string-remove-duplicate-heads-tails.esm.js";
 
