d3e0cb7583fd4eee7691585faddbf51e590a37cc
Roy Revelt <roy@codsen.com>
2026-08-03 05:07:13 +0100

    tests: update tests

--- packages/tsd-extract/test/exports.js
+++ packages/tsd-extract/test/exports.js
@@ -1,8 +1,8 @@
+// biome-ignore-all lint/correctness/noUnusedImports: convenience when writing new tests later
 import { test } from "uvu";
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { equal, is, ok, throws, type, not, match } from "uvu/assert";
+import { equal, is, match, not, ok, throws, type } from "uvu/assert";
 
-import { extract, roysSort as contentSort } from "../dist/tsd-extract.esm.js";
+import { roysSort as contentSort, extract } from "../dist/tsd-extract.esm.js";
 import { mixer } from "./util/util.js";
 
 // -----------------------------------------------------------------------------
