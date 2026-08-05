4e991bff7974442ff4a00fb5e4878f1bef22480d
Roy Revelt <roy@codsen.com>
2026-08-05 05:27:49 +0100

    docs: modernise quick-take example

--- packages/util-array-object-or-both/examples/_quickTake.js
+++ packages/util-array-object-or-both/examples/_quickTake.js
@@ -1,6 +1,6 @@
 // Quick Take
 
-import { strict as assert } from "assert";
+import { strict as assert } from "node:assert";
 
 import { arrObjOrBoth } from "../dist/util-array-object-or-both.esm.js";
 
