c836bfa56a32e995b3c3fa17d470e44dbd2e857f
Roy Revelt <roy@codsen.com>
2026-08-26 18:35:56 +0100

    perf(string-left-right): benchmark paired core lookups

--- packages/string-left-right/perf/check.js
+++ packages/string-left-right/perf/check.js
@@ -2,12 +2,15 @@
 import path from "node:path";
 
 import { runPerf } from "../../../ops/scripts/perf.js";
-import { right } from "../dist/string-left-right.esm.js";
+import { left, right } from "../dist/string-left-right.esm.js";
 
 const callerDir = path.resolve(".");
 
-const testme = () =>
-  right("a \n\n\n     \n      \t\t\t\t      \n \n    \n b", 1);
+const input = "xa \n\n\n     \n      \t\t\t\t      \n \n    \n b";
+const rightmost = input.length - 1;
+
+// Paired core lookup: both calls cross the same mixed-whitespace gap.
+const testme = () => right(input, 1) + left(input, rightmost);
 
 // action
 runPerf(testme, callerDir);
