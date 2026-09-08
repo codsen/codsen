4fc8d5a44b45f1688b03abf214fe5db766cefc35
Roy Revelt <roy@codsen.com>
2026-09-08 10:47:56 +0100

    fix(regex-is-jinja-nunjucks): exclude standalone closing markers

--- packages/regex-is-jinja-nunjucks/src/main.ts
+++ packages/regex-is-jinja-nunjucks/src/main.ts
@@ -3,7 +3,7 @@ import { version as v } from "../package.json";
 const version: string = v;
 
 function isJinjaNunjucksRegex(): RegExp {
-  return /{%|{{|%}|}}/gi;
+  return /{%|{{/gi;
 }
 
 export { isJinjaNunjucksRegex, version };
