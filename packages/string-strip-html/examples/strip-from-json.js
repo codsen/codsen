cf66b8ab0cf146d85d38b8206733a4cac98fd14b
Roy Revelt <roy@codsen.com>
2026-09-19 16:24:22 +0100

    docs(string-strip-html): use consolidated traversal in JSON example

--- packages/string-strip-html/examples/strip-from-json.js
+++ packages/string-strip-html/examples/strip-from-json.js
@@ -1,7 +1,7 @@
 // Strip HTML from a raw JSON string
 
 import { strict as assert } from "node:assert";
-import { traverse } from "ast-monkey-traverse";
+import { traverse } from "ast-monkey/traverse";
 
 import { stripHtml } from "../dist/string-strip-html.esm.js";
 
